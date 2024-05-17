d6708fd539910cdf6819b6b0755e1250d3164bcc
Karl Higley <khigley@umn.edu>
2024-05-17 16:22:24 -0400

    Add classes for `Entity` and `Mention` domain concepts

--- src/poprox_recommender/domain/article.py
+++ src/poprox_recommender/domain/article.py
@@ -1,7 +1,25 @@
 from datetime import datetime, timezone
+from typing import List, Optional
 from uuid import UUID
 
 from pydantic import BaseModel
+
+
+class Entity(BaseModel):
+    entity_id: Optional[UUID] = None
+    external_id: Optional[str] = None
+    name: str
+    entity_type: str
+    source: str
+    raw_data: str
+
+
+class Mention(BaseModel):
+    article_id: Optional[UUID] = None
+    mention_id: Optional[UUID] = None
+    source: str
+    relevance: float
+    entity: Entity
 
 
 class Article(BaseModel):
@@ -10,3 +28,4 @@ class Article(BaseModel):
     content: str = ""
     url: str
     published_at: datetime = datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)
+    mentions: List[Mention] = []
