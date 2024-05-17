dc95ff32114c7384fe7bbe5e6d5be3fae9fbfa02
Karl Higley <khigley@umn.edu>
2024-05-17 09:38:49 -0400

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while getopts ":e:r:" flag
+do
+    case "${flag}" in
+        e) env=${OPTARG};;
+        r) region=${OPTARG};;
+        *) echo "Invalid option. Only -e and -r are allowed" >&2
+            exit 1 ;;
+    esac
+done
+env=${env:-prod}
+region=${region:-us-east-1}
+
+echo "ENV: $env";
+echo "Region: $region";
+
+# Download model artifacts
+dvc pull
+
+# Build container and deploy functions
+serverless deploy --stage "${env}" --region "${region}"
