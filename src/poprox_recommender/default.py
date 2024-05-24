b5a57136ba200b78da25c431951fda792a81174d
imrecommender <Miranda951223@gmail.com>
2024-05-24 01:48:29 -0500

    Change MMR_diversification

--- src/poprox_recommender/default.py
+++ src/poprox_recommender/default.py
@@ -8,7 +8,8 @@ import swifter  # noqa: F401 # pylint: disable=unused-import
 import numpy as np
 import pandas as pd
 import torch as th
-
+import sys
+sys.path.append('../')
 from tqdm import tqdm
 from nltk.tokenize import word_tokenize
 from transformers import AutoTokenizer
@@ -19,7 +20,7 @@ from poprox_recommender.model.nrms import NRMS
 @dataclass
 class ModelConfig:
     num_epochs: float = 10
-    
+
     num_clicked_news_a_user: float = 50
     word_freq_threshold: float = 1
     dropout_probability: float = 0.2
@@ -37,14 +38,11 @@ def parse_row(token_mapping, row):
     new_row = [row.url, []]
 
     try:
-        for i, w in enumerate(word_tokenize(row.title.lower())):
-            if w in token_mapping:
-                new_row[1][i] = token_mapping[w]
         # token_mapping is the name of pre-trained tokenizer
         tokenizer = AutoTokenizer.from_pretrained(token_mapping)
-        new_row[1] = tokenizer.encode(row.title, padding = 'max_length', 
+        new_row[1] = tokenizer.encode(row.title, padding = 'max_length',
                                       max_length = 30, truncation = True)
-        
+
     except IndexError:
         pass
 
@@ -54,20 +52,19 @@ def parse_row(token_mapping, row):
     )
 
 
-# check the input type of articles 
-def is_list_of_dicts(articles): 
+# check the input type of articles
+def is_list_of_dicts(articles):
     if isinstance(articles, list) and all(isinstance(item, dict) for item in articles):
-        return True 
+        return True
     return False
 
 # prepare the news.tsv with information for NRMS model
 def transform_article_features(articles: List[Article], token_mapping):
-    if is_list_of_dicts: 
-        article_df = pd.DataFrame(articles)
-    else:
-        article_df = pd.DataFrame([article.model_dump() for article in articles])
+
+    article_df = pd.DataFrame([article.model_dump() for article in articles])
     article_df.fillna(" ", inplace=True)
     parse_fn = partial(parse_row, token_mapping)
+
     return article_df.swifter.apply(parse_fn, axis=1)
 
 
@@ -98,8 +95,8 @@ def build_article_embeddings(article_features, model, device):
 
 def build_clicks_df(click_history: ClickHistory):
     user_df = pd.DataFrame()
-    user_df["user"] = [str(click_history['account_id'])]
-    user_df["clicked_news"] = [" ".join([str(id_ for id_ in click_history['article_id'])])]
+    user_df["user"] = [str(click_history.account_id)]
+    user_df["clicked_news"] = [" ".join([str(id_) for id_ in click_history.article_id])]
     return user_df
 
 
@@ -116,7 +113,6 @@ def build_user_embeddings(user_df, article_embeddings, model, device, max_clicks
         repeated_times = max_clicks_per_user - len(user["clicked_news"])
         assert repeated_times >= 0
         user["clicked_news"] = ["PADDED_NEWS"] * repeated_times + user["clicked_news"]
-
         default = article_embeddings["PADDED_NEWS"]
         clicked_news_vector = (
             th.stack(
@@ -141,30 +137,33 @@ def build_user_embeddings(user_df, article_embeddings, model, device, max_clicks
 def mmr_diversification(rewards, similarity_matrix, theta, topk):
     # MR_i = \theta * reward_i - (1 - \theta)*max_{j \in S} sim(i, j) # S us
     # R is all candidates (not selected yet)
-    S = []  # final recommendation (topk index)
 
+    S = []  # final recommendation (topk index)
     # first recommended item
     S.append(rewards.argmax())
 
-    for k in range(topk):
-        candidate = None
+    for k in range(topk - 1):
+        candidate = None # next item
         best_MR = float("-inf")
-        for i, reward_i in enumerate(rewards):  # iterate R
+
+        for i, reward_i in enumerate(rewards):  # iterate R for next item
+            if i in S:
+                continue
             max_sim = float("-inf")
 
             for j in S:
                 sim = similarity_matrix[i][j]
-                if sim >= max_sim:
+                if sim > max_sim:
                     max_sim = sim
 
             mr_i = theta * reward_i - (1 - theta) * max_sim
-            if mr_i >= best_MR and i not in S:
+            if mr_i > best_MR:
                 best_MR = mr_i
                 candidate = i
-        if candidate:
-            S.append(candidate)
 
-    return S
+        if candidate != None:
+            S.append(candidate)
+    return S # LIST(candidate index)
 
 
 def generate_recommendations(model, articles, article_vectors, similarity_matrix, user_embeddings, num_slots=10):
@@ -172,7 +171,7 @@ def generate_recommendations(model, articles, article_vectors, similarity_matrix
     for user, user_vector in user_embeddings.items():
         pred = model.get_prediction(article_vectors, user_vector.squeeze())
         pred = pred.cpu().detach().numpy()
-        recs = mmr_diversification(pred, similarity_matrix, theta=0.9, topk=num_slots)
+        recs = mmr_diversification(pred, similarity_matrix, theta=1, topk=num_slots)
         recommendations[user] = [articles[int(rec)] for rec in recs]
     return recommendations
 
@@ -186,13 +185,14 @@ def select_with_model(
     model,
     model_device,
     num_slots=10,
-    max_clicks_per_user=10,
+    max_clicks_per_user=50,
 ):
     # Translate clicks JSON to dataframe
     user_df = build_clicks_df(click_history)
 
     # Build embedding tables
     past_article_embeddings, _ = build_article_embeddings(past_article_features, model, model_device)
+
     user_embeddings = build_user_embeddings(user_df, past_article_embeddings, model, model_device, max_clicks_per_user)
 
     recommendations = generate_recommendations(
@@ -222,11 +222,12 @@ def compute_similarity_matrix(todays_article_vectors):
 def select_articles(
     todays_articles: List[Article],
     past_articles: List[Article],
-    click_histories: List[ClickHistory], 
+    click_histories: List[ClickHistory],
     model_checkpoint,
     model_device,
     token_mapping,
-    num_slots=10,
+    num_slots,
+
 ):
 
     # Transform news to model features
@@ -235,9 +236,9 @@ def select_articles(
     # Extract the set of articles that have actually been clicked
     clicked_article_ids = set()
     for history in click_histories:
-        clicked_article_ids.update(history['article_id'])
+        clicked_article_ids.update(history.article_id)
 
-    clicked_articles = [article for article in past_articles if article['article_id'] in clicked_article_ids]
+    clicked_articles = [article for article in past_articles if article.article_id in clicked_article_ids]
 
     # Convert clicked article attributes into model features
     past_article_features = transform_article_features(
@@ -254,8 +255,8 @@ def select_articles(
 
     recommendations = {}
     for click_history in click_histories:
-        account_id = click_history['account_id']
-        if model_checkpoint and token_mapping and click_history['article_id']:
+        account_id = click_history.account_id
+        if model_checkpoint and token_mapping and click_history.article_id:
             user_recs = select_with_model(
                 todays_articles,
                 todays_article_vectors,
