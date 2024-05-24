cf5f6a23efb927f12621dd917e8f2529698ff1e7
imrecommender <Miranda951223@gmail.com>
2024-05-24 01:48:29 -0500

    Change MMR_diversification

--- src/poprox_recommender/model/nrms/news_encoder.py
+++ src/poprox_recommender/model/nrms/news_encoder.py
@@ -9,36 +9,36 @@ from transformers import AutoConfig, AutoModel
 class NewsEncoder(torch.nn.Module):
     def __init__(self, config):
         super(NewsEncoder, self).__init__()
-        
+
         self.config = config
 
-        
+
         self.plm = AutoModel.from_pretrained(self.config.pretrained_model)
-        
+
         plm_hidden_size = AutoConfig.from_pretrained(self.config.pretrained_model).hidden_size
-        
+
         self.multihead_attention = nn.MultiheadAttention(
-            embed_dim = plm_hidden_size, 
-            num_heads = self.config.num_attention_heads, 
-            batch_first = True) 
-        
-        
+            embed_dim = plm_hidden_size,
+            num_heads = self.config.num_attention_heads,
+            batch_first = True)
+
+
         self.additive_attention = NewsAdditiveAttention(plm_hidden_size, self.config.additive_attn_hidden_dim)
-       
+
     def forward(self, news_input):
-  
+
         # batch_size, num_words_title, word_embedding_dim
 
-        print('news encoder', news_input)
+
         V = self.plm(news_input).last_hidden_state
         multihead_attn_output, _ = self.multihead_attention(
             V, V, V
         )  # [batch_size, seq_len, hidden_size] -> [batch_size, seq_len, hidden_size]
-        
+
         additive_attn_output = self.additive_attention(
             multihead_attn_output
         )  # [batch_size, seq_len, hidden_size] -> [batch_size, seq_len, hidden_size]
-        
+
         output = torch.sum(
             additive_attn_output, dim=1
         )  # [batch_size, seq_len, hidden_size] -> [batch_size, hidden_size]
