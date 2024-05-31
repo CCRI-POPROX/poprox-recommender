f1fd2e05d745f6b3e76d73330047d9cc8cbd5db0
Karl Higley <khigley@umn.edu>
2024-05-31 14:31:47 -0400

    Use `/tmp/` for all `transformers` cache dirs
    Otherwise this code won't run in a Lambda, where the file system is ready-only except for `/tmp/`

--- src/poprox_recommender/model/nrms/news_encoder.py
+++ src/poprox_recommender/model/nrms/news_encoder.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from poprox_recommender.model.general.attention.newsadditive import NewsAdditiveAttention
+from poprox_recommender.model.general.attention.newsadditive import (
+    NewsAdditiveAttention,
+)
 from transformers import AutoConfig, AutoModel
-
 
 
 class NewsEncoder(torch.nn.Module):
@@ -12,23 +13,27 @@ class NewsEncoder(torch.nn.Module):
 
         self.config = config
 
+        self.plm = AutoModel.from_pretrained(
+            self.config.pretrained_model, cache_dir="/tmp/"
+        )
 
-        self.plm = AutoModel.from_pretrained(self.config.pretrained_model)
-
-        plm_hidden_size = AutoConfig.from_pretrained(self.config.pretrained_model).hidden_size
+        plm_hidden_size = AutoConfig.from_pretrained(
+            self.config.pretrained_model, cache_dir="/tmp/"
+        ).hidden_size
 
         self.multihead_attention = nn.MultiheadAttention(
-            embed_dim = plm_hidden_size,
-            num_heads = self.config.num_attention_heads,
-            batch_first = True)
+            embed_dim=plm_hidden_size,
+            num_heads=self.config.num_attention_heads,
+            batch_first=True,
+        )
 
-
-        self.additive_attention = NewsAdditiveAttention(plm_hidden_size, self.config.additive_attn_hidden_dim)
+        self.additive_attention = NewsAdditiveAttention(
+            plm_hidden_size, self.config.additive_attn_hidden_dim
+        )
 
     def forward(self, news_input):
 
         # batch_size, num_words_title, word_embedding_dim
-
 
         V = self.plm(news_input).last_hidden_state
         multihead_attn_output, _ = self.multihead_attention(
