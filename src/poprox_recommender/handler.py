bc5e3cba2dcc1010f0911cd6e86498598c32d19c
Karl Higley <khigley@umn.edu>
2024-06-04 11:48:11 -0400

    Deploy the recommender as a Lambda-based endpoint (#14)
    This sets up the relevant configuration to deploy the recommender as an
    independent endpoint that the platform can call instead of directly
    importing code from this repo. Along the way, it also removes
    `requirements.txt`, fixes some dependency issues, properly validates API
    inputs, and slims the Docker image down to a tenth of its original size
    which also speeds up deploys.
    
    ---------
    
    Co-authored-by: Daniel Kluver <kluve018@umn.edu>

--- src/poprox_recommender/handler.py
+++ src/poprox_recommender/handler.py
@@ -3,6 +3,7 @@ import json
 import torch as th
 from safetensors.torch import load_file
 
+from poprox_concepts import Article, ClickHistory
 from poprox_recommender.default import select_articles
 from poprox_recommender.paths import project_root
 
@@ -18,17 +19,24 @@ def load_model(device_name=None):
     checkpoint = load_file(load_path)
     return checkpoint, device_name
 
+
 MODEL, DEVICE = load_model()
-TOKEN_MAPPING = 'distilbert-base-uncased' # can be modified
+TOKEN_MAPPING = "distilbert-base-uncased"  # can be modified
 
 
 def generate_recs(event, context):
-    request_body = json.loads(event["body"])
+    req_body = json.loads(event["body"])
 
-    todays_articles = request_body["todays_articles"]
-    past_articles = request_body["past_articles"]
-    click_data = request_body["click_data"]
-    num_recs = request_body["num_recs"]
+    todays_articles = [
+        Article.model_validate(attrs) for attrs in req_body["todays_articles"]
+    ]
+    past_articles = [
+        Article.model_validate(attrs) for attrs in req_body["past_articles"]
+    ]
+    click_data = [
+        ClickHistory.model_validate(attrs) for attrs in req_body["click_data"]
+    ]
+    num_recs = req_body["num_recs"]
 
     recommendations = select_articles(
         todays_articles,
@@ -39,8 +47,14 @@ def generate_recs(event, context):
         TOKEN_MAPPING,
         num_recs,
     )
+
     body = {
-        "recommendations": recommendations,
+        "recommendations": {
+            str(account_id): [
+                json.loads(article.model_dump_json()) for article in articles
+            ]
+            for account_id, articles in recommendations.items()
+        },
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
