5a1b65590e8c552f19240f59ee4f05469867ca1f
Michael Ekstrand <md@ekstrandom.net>
2024-06-12 10:19:04 -0400

    Improve documentation and logging for paths.py

--- src/poprox_recommender/paths.py
+++ src/poprox_recommender/paths.py
@@ -1,12 +1,20 @@
+# pyright: strict
+from __future__ import annotations
+
 import logging
 import os
-from pathlib import Path
 import tomllib
+from pathlib import Path
+from typing import overload
 
 _logger = logging.getLogger(__name__)
 
 
-def project_root(require: bool = True) -> Path | None:
+@overload
+def project_root() -> Path: ...
+@overload
+def project_root(*, require: bool) -> Path | None: ...
+def project_root(*, require: bool = True) -> Path | None:
     """
     Find the project root directory (when we are running in the project).
 
@@ -20,6 +28,10 @@ def project_root(require: bool = True) -> Path | None:
             Whether to fail when the project root is not found, or return
             ``None``. If ``require=False`` this function will stil fail on a
             *defective* project root (contains an invalid ``pyproject.toml``).
+
+    Returns:
+        The full path to the project root directory.  If the project root is
+        not found and ``require=False``, returns ``None``.
     """
     cwd = Path(".").resolve()
     candidate = cwd
@@ -44,11 +56,17 @@ def model_file_path(name: str) -> Path:
     * The ``models`` directory under the :func:`project_root`.
     * ``$CONDA_PREFIX/models`` (if env var ``CONDA_PREFIX`` is defined, which is
       done by ``conda activate``).
+
+    Args:
+        name: The path to the model file (or directory), relative to ``models``.
+
+    Returns:
+        The full path to the model file, if it exists.
     """
-    model_dirs = []
+    model_dirs: list[Path] = []
     if "POPROX_MODELS" in os.environ:
         model_dirs.append(Path(os.environ["POPROX_MODELS"]))
-    root = project_root(False)
+    root = project_root(require=False)
     if root is not None:
         model_dirs.append(root / "models")
     if "CONDA_PREFIX" in os.environ:
