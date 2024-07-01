5ee197fa2fb7267bf7f4880400c520845b9ff88d
Michael Ekstrand <md@ekstrandom.net>
2024-07-01 13:56:47 -0400

    comment conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+# PyTest runs this file before even attempting to import any of the test cases,
+# so in addition to dynamic PyTest configuration, we can also put code to set
+# up import paths and things like that here.
 import os
 import sys
 from pathlib import Path
@@ -11,4 +14,5 @@ try:
 except ImportError:
     # tweak up paths so pytest can work without installing the recommender
     sys.path.insert(0, os.fspath(src_dir))
+    # put in `PYTHONPATH` too so that serverless can find our python code
     os.environ["PYTHONPATH"] = os.fspath(src_dir)
