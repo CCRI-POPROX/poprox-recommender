e05ae6d69bcdd02f17bf12bc477cf71bd4477b53
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-03 14:15:44 -0400

    rename model-name constants

--- src/poprox_recommender/topics.py
+++ src/poprox_recommender/topics.py
@@ -8,7 +8,7 @@ from poprox_concepts import Article, ClickHistory
 from poprox_concepts.domain.topics import GENERAL_TOPICS
 from poprox_recommender.paths import model_file_path
 
-NEWS_MODEL = "news-category-classifier-distilbert"
+NEWS_MODEL_NAME = "news-category-classifier-distilbert"
 
 
 def extract_general_topics(article: Article):
@@ -64,9 +64,9 @@ def classify_news_topic(model, tokenizer, general_topics, topic):
 def match_news_topics_to_general(articles: list[Article]):
     # Load the pre-trained tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained(
-        model_file_path(NEWS_MODEL)
+        model_file_path(NEWS_MODEL_NAME)
     )  # a highly downloaded fine-tuned model on news
-    model = AutoModel.from_pretrained(model_file_path(NEWS_MODEL))
+    model = AutoModel.from_pretrained(model_file_path(NEWS_MODEL_NAME))
 
     topic_matched_dict = {}  # we might be able to connect this to DB to read previous matching?
     article_to_new_topic = {}
