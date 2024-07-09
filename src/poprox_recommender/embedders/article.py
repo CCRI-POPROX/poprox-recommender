175d9ae629adea063d5bc90d60966e5f461416ce
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-09 16:33:44 -0400

    Cache article embeddings (#66)
    This adds a cache to cache the article embeddings in a pipeline instance
    to reduce recomputation in evaluation.
    
    It also adds a `datachecks` module with a function to do assertion
    checks on tensor sizes with useful error messages.

--- src/poprox_recommender/embedders/article.py
+++ src/poprox_recommender/embedders/article.py
@@ -1,25 +1,81 @@
+# pyright: basic
+import logging
+from typing import Protocol
+from uuid import UUID
+
 import torch as th
+from transformers import PreTrainedTokenizer
 
 from poprox_concepts import ArticleSet
+from poprox_recommender.datachecks import assert_tensor_size
+
+logger = logging.getLogger(__name__)
+TITLE_LENGTH_LIMIT = 30
+
+
+class ArticleEmbeddingModel(Protocol):
+    """
+    Interface exposed by article embedding models.
+    """
+
+    def get_news_vector(self, news: th.Tensor) -> th.Tensor: ...
 
 
 class ArticleEmbedder:
-    def __init__(self, model, tokenizer, device):
+    model: ArticleEmbeddingModel
+    tokenizer: PreTrainedTokenizer
+    device: str | None
+    embedding_cache: dict[UUID, th.Tensor]
+
+    def __init__(self, model: ArticleEmbeddingModel, tokenizer: PreTrainedTokenizer, device: str | None):
         self.model = model
         self.tokenizer = tokenizer
         self.device = device
+        self.embedding_cache = {}
 
     def __call__(self, article_set: ArticleSet) -> ArticleSet:
-        tokenized_titles = {}
-        for article in article_set.articles:
-            tokenized_titles[article.article_id] = self.tokenizer.encode(
-                article.title, padding="max_length", max_length=30, truncation=True
+        # Step 1: get the cached articles wherever possible.
+        # Since Python dictionaries preserve order, this keeps the order aligned with the
+        # input article set.
+        cached = {article.article_id: self.embedding_cache.get(article.article_id) for article in article_set.articles}
+
+        # Step 2: find the uncached articles.
+        uncached = [article for article in article_set.articles if cached[article.article_id] is None]
+
+        if uncached:
+            logger.debug("need to embed %d of %d articles", len(uncached), len(cached))
+            # Step 3: tokenize the uncached articles
+            uc_title_tokens = th.stack(
+                [
+                    th.tensor(
+                        self.tokenizer.encode(
+                            article.title, padding="max_length", max_length=TITLE_LENGTH_LIMIT, truncation=True
+                        ),
+                        dtype=th.int32,
+                    )
+                    for article in uncached
+                ]
             )
+            assert_tensor_size(uc_title_tokens, len(uncached), TITLE_LENGTH_LIMIT)
 
-        title_tensor = th.tensor(list(tokenized_titles.values())).to(self.device)
-        if len(title_tensor.shape) == 1:
-            title_tensor = title_tensor.unsqueeze(dim=0)
+            # Step 4: embed the uncached articles
+            uc_embeddings = self.model.get_news_vector(uc_title_tokens)
+            assert uc_embeddings.shape[0] == len(uncached)
 
-        article_set.embeddings = self.model.get_news_vector(title_tensor)
+            # Step 5: store embeddings to cache & result
+            for i, uca in enumerate(uncached):
+                # copy the tensor so it isn't attached to excess memory
+                a_emb = uc_embeddings[i, :].clone()
+                cached[uca.article_id] = a_emb
+                self.embedding_cache[uca.article_id] = a_emb
+
+        # Step 6: stack the embeddings into a single tensor
+        embed_single_tensors = list(cached.values())
+        assert not any(e is None for e in embed_single_tensors)
+        embed_tensor = th.stack(embed_single_tensors)  # type: ignore
+        assert embed_tensor.shape[0] == len(article_set.articles)
+
+        # Step 7: put the embedding tensor on the output
+        article_set.embeddings = embed_tensor  # type: ignore
 
         return article_set
---
+++ tests/test_datachecks.py
@@ -0,0 +1,27 @@
+import torch as th
+from pytest import raises
+
+from poprox_recommender.datachecks import assert_tensor_size
+
+
+def test_tensor_size_ok():
+    assert_tensor_size(th.randn(10, 50), 10, 50)
+
+
+def test_tensor_size_fail():
+    with raises(AssertionError, match=r"^unexpected tensor size"):
+        assert_tensor_size(th.randn(10, 40), 10, 50)
+
+
+def test_tensor_size_fail_label():
+    with raises(AssertionError, match=r"^test: unexpected tensor size"):
+        assert_tensor_size(th.randn(10, 40), 10, 50, label="test")
+
+
+def test_tensor_size_prefix():
+    assert_tensor_size(th.randn(10, 50, 25), 10, 50, label="test")
+
+
+def test_tensor_size_no_prefix():
+    with raises(AssertionError, match=r"^test: unexpected tensor size"):
+        assert_tensor_size(th.randn(10, 50, 25), 10, 50, label="test", prefix=False)
