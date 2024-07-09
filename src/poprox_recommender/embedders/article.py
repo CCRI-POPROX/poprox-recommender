06f05aafb399cf260a30fa7d12fcfd943396b105
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-09 17:59:37 -0400

    move cached articles to gpu

--- src/poprox_recommender/embedders/article.py
+++ src/poprox_recommender/embedders/article.py
@@ -80,9 +80,8 @@ class ArticleEmbedder:
 
         # Step 6: stack the embeddings into a single tensor
         # we do this with a list to properly deal with duplicate articles
-        embed_single_tensors = [cached[article.article_id] for article in article_set.articles]
-        assert not any(e is None for e in embed_single_tensors)
-        embed_tensor = th.stack(embed_single_tensors).to(self.device)  # type: ignore
+        embed_single_tensors = [cached[article.article_id].to(self.device) for article in article_set.articles]  # type: ignore
+        embed_tensor = th.stack(embed_single_tensors)  # type: ignore
         assert_tensor_size(
             embed_tensor, len(article_set.articles), self.model.embedding_size, label="final article embeddings"
         )
