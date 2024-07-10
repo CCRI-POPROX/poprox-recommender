b7b451e4b96a543ad12cb34f25208ed7467daab3
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-10 15:53:19 +0000

    and a torch_inference decorator

--- src/poprox_recommender/torch/__init__.py
+++ src/poprox_recommender/torch/__init__.py
@@ -3,5 +3,6 @@ Utility functions for implementing POPROX recommender components with PyTorch.
 """
 
 from .datachecks import assert_tensor_size
+from .decorators import torch_inference
 
-__all__ = ["assert_tensor_size"]
+__all__ = ["assert_tensor_size", "torch_inference"]
