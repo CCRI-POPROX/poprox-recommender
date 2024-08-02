0489788310e1824769f9aa19ab0095b6b9731f89
sophiasun0515 <sophiasun0515@gmail.com>
2024-08-02 13:57:44 +0000

    further optimization, running time ~12hr

--- src/poprox_recommender/evaluation/evaluate.py
+++ src/poprox_recommender/evaluation/evaluate.py
@@ -6,9 +6,11 @@ import logging
 import numpy as np
 import pandas as pd
 from lenskit.metrics import topn
+from line_profiler import LineProfiler
 from tqdm import tqdm
 
 from poprox_concepts.api.recommendations import RecommendationRequest
+from poprox_concepts.domain import Article, ArticleSet
 from poprox_recommender.data.mind import MindData
 from poprox_recommender.evaluation.metrics import rank_biased_overlap
 from poprox_recommender.paths import project_root
@@ -16,21 +18,28 @@ from poprox_recommender.paths import project_root
 logger = logging.getLogger("poprox_recommender.measurement_offline")
 
 
+def convert_df_to_article_set(rec_df):
+    articles = []
+    for _, row in rec_df.iterrows():
+        articles.append(Article(article_id=row["item"], title=""))
+    return ArticleSet(articles=articles)
+
+
 def compute_rec_metric(recs_df: pd.DataFrame, request: RecommendationRequest):
     # Make sure you run generate.py first to get the recommendations data file
-    recs = recs_df[recs_df["user"] == request.interest_profile.profile_id]
+    recs = recs_df.loc[str(request.interest_profile.profile_id)]
     truth = mind_data.user_truth(request.interest_profile.profile_id)
 
     personalized = 1 if request.interest_profile.click_history.article_ids else 0
 
     single_rr = topn.recip_rank(recs[recs["stage"] == "final"], truth[truth["rating"] > 0])
-    single_ndcg5 = topn.ndcg(recs, truth, k=5)
-    single_ndcg10 = topn.ndcg(recs, truth, k=10)
+    single_ndcg5 = topn.ndcg(recs[recs["stage"] == "final"], truth, k=5)
+    single_ndcg10 = topn.ndcg(recs[recs["stage"] == "final"], truth, k=10)
 
-    ranked = recs[recs["stage"] == "ranked"]
-    reranked = recs[recs["stage"] == "reranked"]
+    ranked = convert_df_to_article_set(recs[recs["stage"] == "ranked"])
+    reranked = convert_df_to_article_set(recs[recs["stage"] == "reranked"])
 
-    if not ranked.empty and not reranked.empty:
+    if ranked and reranked:
         single_rbo5 = rank_biased_overlap(ranked, reranked, k=5)
         single_rbo10 = rank_biased_overlap(ranked, reranked, k=10)
     else:
@@ -61,7 +70,11 @@ if __name__ == "__main__":
     rbo5 = []
     rbo10 = []
 
+    recs_df.set_index("user", inplace=True)
     for request in tqdm(mind_data.iter_users(), total=mind_data.n_users, desc="evaluate"):
+        lp = LineProfiler()
+        lp_wrapper = lp(compute_rec_metric)
+        lp.print_stats()
         single_ndcg5, single_ndcg10, single_rr, single_rbo5, single_rbo10, personalized = compute_rec_metric(
             recs_df, request
         )
