662f6c3776c2c4be2f5e177ec979e3fdefe197de
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-02 17:35:21 -0400

    fix outputs to get tests working

--- src/poprox_recommender/default.py
+++ src/poprox_recommender/default.py
@@ -1,19 +1,27 @@
 # pyright: basic
 import logging
+from dataclasses import dataclass
 from typing import Any
 
-from poprox_concepts import ArticleSet, InterestProfile
+from poprox_concepts import Article, ArticleSet, InterestProfile
 from poprox_recommender.components.diversifiers import MMRDiversifier, PFARDiversifier, TopicCalibrator
 from poprox_recommender.components.embedders import ArticleEmbedder, UserEmbedder
 from poprox_recommender.components.filters import TopicFilter
 from poprox_recommender.components.rankers.topk import TopkRanker
 from poprox_recommender.components.samplers import UniformSampler
 from poprox_recommender.components.scorers import ArticleScorer
+from poprox_recommender.lkpipeline import Pipeline
 from poprox_recommender.model import get_model
-from poprox_recommender.pipeline import PipelineState, RecommendationPipeline
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+@dataclass
+class Recommendations:
+    recs: list[Article]
+    initial: ArticleSet | None = None
+    reranked: ArticleSet | None = None
 
 
 def select_articles(
@@ -22,7 +30,7 @@ def select_articles(
     interest_profile: InterestProfile,
     num_slots: int,
     algo_params: dict[str, Any] | None = None,
-) -> PipelineState:
+) -> Recommendations:
     """
     Select articles with default recommender configuration.
     """
@@ -34,16 +42,25 @@ def select_articles(
     if pipeline is None:
         pipeline = fallback_pipeline(num_slots)
 
-    inputs = {
-        "candidate": candidate_articles,
-        "clicked": clicked_articles,
-        "profile": interest_profile,
-    }
+    rank = pipeline.node("rank")
+    topk = pipeline.node("rank-topk", missing="none")
+    if topk is None:
+        wanted = (rank,)
+    else:
+        wanted = (rank, topk)
 
-    return pipeline(inputs)
+    result = pipeline.run(*wanted, candidate=candidate_articles, clicked=clicked_articles, profile=interest_profile)
+
+    if topk is None:
+        assert isinstance(result, ArticleSet)
+        return Recommendations(result.articles, initial=result)
+    else:
+        assert isinstance(result, tuple)
+        reranked, initial = result
+        return Recommendations(reranked.articles, initial=initial, reranked=reranked)
 
 
-def personalized_pipeline(num_slots: int, algo_params: dict[str, Any] | None = None) -> RecommendationPipeline | None:
+def personalized_pipeline(num_slots: int, algo_params: dict[str, Any] | None = None) -> Pipeline | None:
     """
     Create the default personalized recommendation pipeline.
 
@@ -82,18 +99,26 @@ def personalized_pipeline(num_slots: int, algo_params: dict[str, Any] | None = N
         logger.info("Recommendations will be ranked with plain top-k.")
         ranker = topk_ranker
 
-    pipeline = RecommendationPipeline(name=diversify)
-    pipeline.add(article_embedder, inputs=["candidate"], output="candidate")
-    pipeline.add(article_embedder, inputs=["clicked"], output="clicked")
-    pipeline.add(user_embedder, inputs=["clicked", "profile"], output="profile")
-    pipeline.add(article_scorer, inputs=["candidate", "profile"], output="candidate")
-    pipeline.add(topk_ranker, inputs=["candidate", "profile"], output="ranked")
-    pipeline.add(ranker, inputs=["candidate", "profile"], output="reranked")
+    pipeline = Pipeline()
+    candidates = pipeline.create_input("candidate", ArticleSet)
+    clicked = pipeline.create_input("clicked", ArticleSet)
+    profile = pipeline.create_input("profile", InterestProfile)
+    e_cand = pipeline.add_component("embed-candidates", article_embedder, article_set=candidates)
+    e_click = pipeline.add_component("embed-clicked-articles", article_embedder, article_set=clicked)
+    e_user = pipeline.add_component("embed-user", user_embedder, clicked_articles=e_click, interest_profile=profile)
+    scored = pipeline.add_component(
+        "score-articles", article_scorer, candidate_articles=e_cand, interest_profile=e_user
+    )
+    topk = pipeline.add_component("rank-topk", topk_ranker, candidate_articles=scored, interest_profile=e_user)
+    if ranker is topk_ranker:
+        pipeline.alias("rank", topk)
+    else:
+        _final = pipeline.add_component("rank", ranker, candidate_articles=scored, interest_profile=e_user)
 
     return pipeline
 
 
-def fallback_pipeline(num_slots: int) -> RecommendationPipeline:
+def fallback_pipeline(num_slots: int) -> Pipeline:
     """
     Create the fallback (non-personalized) pipeline.
 
@@ -103,7 +128,10 @@ def fallback_pipeline(num_slots: int) -> RecommendationPipeline:
     topic_filter = TopicFilter()
     sampler = UniformSampler(num_slots=num_slots)
 
-    pipeline = RecommendationPipeline(name="random_topical")
-    pipeline.add(topic_filter, inputs=["candidate", "profile"], output="topical")
-    pipeline.add(sampler, inputs=["topical", "candidate"], output="ranked")
+    pipeline = Pipeline()
+    candidates = pipeline.create_input("candidate", ArticleSet)
+    _clicked = pipeline.create_input("clicked", ArticleSet)
+    profile = pipeline.create_input("profile", InterestProfile)
+    filtered = pipeline.add_component("filter-articles", topic_filter, candidate=candidates, interest_profile=profile)
+    _sampled = pipeline.add_component("rank", sampler, candidate=filtered, backup=candidates)
     return pipeline
