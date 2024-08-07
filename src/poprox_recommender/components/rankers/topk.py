778da49a9d2e4ac859b4b2dc33ddcf6b3aefaace
Karl Higley <khigley@umn.edu>
2024-08-07 16:32:47 -0400

    Rework the rankers/diversifiers to take explicit parameters

--- src/poprox_recommender/components/rankers/topk.py
+++ src/poprox_recommender/components/rankers/topk.py
@@ -1,14 +1,10 @@
-from typing import Any
-
 import numpy as np
 
 from poprox_concepts import ArticleSet, InterestProfile
-from poprox_recommender.components.rankers import Ranker
 
 
-class TopkRanker(Ranker):
-    def __init__(self, algo_params: dict[str, Any], num_slots=10):
-        self.validate_algo_params(algo_params, [])
+class TopkRanker:
+    def __init__(self, num_slots=10):
         self.num_slots = num_slots
 
     def __call__(self, candidate_articles: ArticleSet, interest_profile: InterestProfile) -> ArticleSet:
