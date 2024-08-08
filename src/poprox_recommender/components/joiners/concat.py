bc6f5b40d8334ac0bd77cdc221f8c71cbd0e9d7b
Karl Higley <khigley@umn.edu>
2024-08-08 15:53:11 -0400

    Improve the legibility of `Concatenate` with a docstring and some tweaks

--- src/poprox_recommender/components/joiners/concat.py
+++ src/poprox_recommender/components/joiners/concat.py
@@ -3,7 +3,17 @@ from poprox_concepts import ArticleSet
 
 class Concatenate:
     def __call__(self, candidates1: ArticleSet, candidates2: ArticleSet) -> ArticleSet:
-        reverse_articles = (candidates1.articles + candidates2.articles)[::-1]
+        """
+        Concatenates two sets of candidates, while deduplicating them, keeping the
+        first occurrence of each article (by id), and maintaining their original order.
+
+        This is achieved by inserting articles into a dict in reverse order, so that
+        articles from the second candidate set are written first and then overwritten
+        by articles from the first candidate set (if there are collisions.) Afterward,
+        the dict keys can be ignored and the dict values are the deduplicated candidates
+        in reverse order. Reversing them one more time returns them to the original order.
+        """
+        reverse_articles = reversed(candidates1.articles + candidates2.articles)
         articles = {article.article_id: article for article in reverse_articles}
 
-        return ArticleSet(articles=list(articles.values())[::-1])
+        return ArticleSet(articles=list(reversed(articles.values())))
