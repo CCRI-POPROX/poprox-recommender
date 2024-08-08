39fec5a29db00c08de68e23729378f5acbddbe41
Karl Higley <khigley@umn.edu>
2024-08-08 15:55:00 -0400

    Simplify the implementation of `Fill`

--- src/poprox_recommender/components/joiners/fill.py
+++ src/poprox_recommender/components/joiners/fill.py
@@ -6,10 +6,5 @@ class Fill:
         self.num_slots = num_slots
 
     def __call__(self, candidates1: ArticleSet, candidates2: ArticleSet) -> ArticleSet:
-        # Fill as many slots as you can from candidates1
-        articles = candidates1.articles[: self.num_slots]
-
-        # Fill out remaining slots from candidates2
-        articles += candidates2.articles[: self.num_slots - len(articles)]
-
-        return ArticleSet(articles=articles)
+        articles = candidates1.articles + candidates2.articles
+        return ArticleSet(articles=articles[: self.num_slots])
