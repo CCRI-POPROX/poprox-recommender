c80507e7c01bbafa4aa23d2a5dc840343c9356cd
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-12 17:22:00 -0400

    add configuration for CPU parallelism

--- src/poprox_recommender/config.py
+++ src/poprox_recommender/config.py
@@ -12,3 +12,32 @@ def default_device() -> str:
     variable is not set.
     """
     return os.environ.get("POPROX_REC_DEVICE", "cpu")
+
+
+def available_cpu_parallelism(max: int | None = None) -> int:
+    """
+    Get the available amount of CPU parallelism.  If the ``POPROX_CPU_COUNT``
+    environment variable is set, that is consulted; otherwise the number of CPUs
+    detected on the system is returned.
+
+    Args:
+        max:
+            The maximum # of CPUs (ignored if CPU count comes from the
+            environment variable).
+
+    Returns:
+        # The number of CPUs to use for parallelism.
+    """
+
+    if "POPROX_CPU_COUNT" in os.environ:
+        return int(os.environ("POPROX_CPU_COUNT"))
+
+    try:
+        # linux allows cpu limits
+        n_cpus = len(os.sched_getaffinity(0))
+    except AttributeError:
+        n_cpus = os.cpu_count()
+    if n_cpus is None:
+        n_cpus = 1
+
+    return n_cpus
