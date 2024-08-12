f1d973970fcf66d48a51ebb55d5eddceb663f2e4
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-12 17:22:11 -0400

    parallelize measurement

--- src/poprox_recommender/evaluation/evaluate.py
+++ src/poprox_recommender/evaluation/evaluate.py
@@ -16,10 +16,13 @@ Options:
             the name of the evaluation run [default: mind-val]
 """
 
+# pyright: basic
 import csv
 import gzip
 import json
 import logging
+from concurrent.futures import ProcessPoolExecutor
+from typing import Iterator, NamedTuple
 from uuid import UUID
 
 import numpy as np
@@ -30,12 +33,19 @@ from tqdm import tqdm
 
 from poprox_concepts.api.recommendations import RecommendationRequest
 from poprox_concepts.domain import Article, ArticleSet
+from poprox_recommender.config import available_cpu_parallelism
 from poprox_recommender.data.mind import MindData
 from poprox_recommender.evaluation.metrics import rank_biased_overlap
 from poprox_recommender.logging_config import setup_logging
 from poprox_recommender.paths import project_root
 
 logger = logging.getLogger("poprox_recommender.evaluation.evaluate")
+
+
+class UserRecs(NamedTuple):
+    request: RecommendationRequest
+    recs: pd.DataFrame
+    truth: pd.DataFrame
 
 
 def convert_df_to_article_set(rec_df):
@@ -45,11 +55,10 @@ def convert_df_to_article_set(rec_df):
     return ArticleSet(articles=articles)
 
 
-def compute_rec_metric(user_recs: dict[UUID, pd.DataFrame], request: RecommendationRequest):
+def compute_rec_metric(user: UserRecs):
     # Make sure you run generate.py first to get the recommendations data file
-    recs = user_recs[request.interest_profile.profile_id]
-    truth = mind_data.user_truth(request.interest_profile.profile_id)
     # Convert truth index from UUID to string
+    request, recs, truth = user
     truth.index = truth.index.astype(str)
 
     personalized = 1 if request.interest_profile.click_history.article_ids else 0
@@ -72,11 +81,33 @@ def compute_rec_metric(user_recs: dict[UUID, pd.DataFrame], request: Recommendat
         single_rbo5 = None
         single_rbo10 = None
 
-    return single_ndcg5, single_ndcg10, single_rr, single_rbo5, single_rbo10, personalized
+    return (
+        request.interest_profile.profile_id,
+        single_ndcg5,
+        single_ndcg10,
+        single_rr,
+        single_rbo5,
+        single_rbo10,
+        personalized,
+    )
+
+
+def rec_users(mind_data: MindData, user_recs: dict[UUID, pd.DataFrame]) -> Iterator[UserRecs]:
+    """
+    Iterate over rec users, yielding each request with its recommendations and
+    truth.  This supports parallel computation of the final metrics.
+    """
+    for request in tqdm(mind_data.iter_users(), total=mind_data.n_users, desc="evaluate"):
+        user_id = request.interest_profile.profile_id
+        assert user_id is not None
+        recs = user_recs[user_id]
+        truth = mind_data.user_truth(user_id)
+        assert truth is not None
+        yield UserRecs(request, recs, truth)
 
 
 def main():
-    options = docopt(__doc__)
+    options = docopt(__doc__)  # type: ignore
     setup_logging(verbose=options["--verbose"], log_file=options["--log-file"])
 
     global mind_data
@@ -87,7 +118,7 @@ def main():
     recs_fn = project_root() / "outputs" / f"{eval_name}-recommendations.parquet"
     logger.info("loading recommendations from %s", recs_fn)
     recs_df = pd.read_parquet(recs_fn)
-    user_recs = dict((UUID(u), df) for (u, df) in recs_df.groupby("user"))
+    user_recs = dict((UUID(str(u)), df) for (u, df) in recs_df.groupby("user"))
     del recs_df
     logger.info("loaded recommendations for %d users", len(user_recs))
 
@@ -106,38 +137,41 @@ def main():
     rbo5 = []
     rbo10 = []
 
-    for request in tqdm(mind_data.iter_users(), total=mind_data.n_users, desc="evaluate"):
-        single_ndcg5, single_ndcg10, single_rr, single_rbo5, single_rbo10, personalized = compute_rec_metric(
-            user_recs, request
-        )
-        user_csv.writerow(
-            [
-                request.interest_profile.profile_id,
-                personalized,
+    with ProcessPoolExecutor(available_cpu_parallelism(8)) as pool:
+        for metrics in tqdm(
+            pool.map(compute_rec_metric, rec_users(mind_data, user_recs), chunksize=1000),
+            total=mind_data.n_users,
+            desc="evaluate",
+        ):
+            user_id, single_ndcg5, single_ndcg10, single_rr, single_rbo5, single_rbo10, personalized = metrics
+            user_csv.writerow(
+                [
+                    user_id,
+                    personalized,
+                    single_ndcg5,
+                    single_ndcg10,
+                    single_rr,
+                    single_rbo5,
+                    single_rbo10,
+                ]
+            )
+            # recommendations {account id (uuid): LIST[Article]}
+            logger.debug(
+                "user %s: NDCG@5=%0.3f, NDCG@10=%0.3f, RR=%0.3f, RBO@5=%0.3f, RBO@10=%0.3f",
+                user_id,
                 single_ndcg5,
                 single_ndcg10,
                 single_rr,
-                single_rbo5,
-                single_rbo10,
-            ]
-        )
-        # recommendations {account id (uuid): LIST[Article]}
-        logger.debug(
-            "user %s: NDCG@5=%0.3f, NDCG@10=%0.3f, RR=%0.3f, RBO@5=%0.3f, RBO@10=%0.3f",
-            request.interest_profile.profile_id,
-            single_ndcg5,
-            single_ndcg10,
-            single_rr,
-            single_rbo5 or -1.0,
-            single_rbo10 or -1.0,
-        )
+                single_rbo5 or -1.0,
+                single_rbo10 or -1.0,
+            )
 
-        ndcg5.append(single_ndcg5)
-        ndcg10.append(single_ndcg10)
-        recip_rank.append(single_rr)
-        rbo5.append(single_rbo5 or np.nan)
-        rbo10.append(single_rbo10 or np.nan)
-        ngood += 1
+            ndcg5.append(single_ndcg5)
+            ndcg10.append(single_ndcg10)
+            recip_rank.append(single_rr)
+            rbo5.append(single_rbo5 or np.nan)
+            rbo10.append(single_rbo10 or np.nan)
+            ngood += 1
 
     user_out.close()
 
