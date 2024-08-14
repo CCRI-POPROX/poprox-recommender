d2e41cb193f6e09728b698511d419962316ca903
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-14 13:50:02 -0400

    sample articles, it's ok

--- src/poprox_recommender/components/samplers/uniform.py
+++ src/poprox_recommender/components/samplers/uniform.py
@@ -12,18 +12,17 @@ class UniformSampler(Component):
         articles = {a.article_id: a for a in candidate.articles}
 
         if backup.articles:
-            backup_articles = {a.article_id: a for a in backup.articles if a.article_id not in articles}
+            backup_articles = [a for a in backup.articles if a.article_id not in articles]
         else:
             backup_articles = []
 
         # we want to sample article IDs for performance
 
-        sampled = random.sample(list(articles.keys()), min(self.num_slots, len(candidate.articles)))
+        sampled = random.sample(candidate.articles, min(self.num_slots, len(candidate.articles)))
 
         if len(sampled) < self.num_slots and backup_articles:
             # add backups to articles, but allow articles to override
             num_backups = min(self.num_slots - len(sampled), len(backup_articles))
-            sampled += random.sample(list(backup_articles.keys()), num_backups)
+            sampled += random.sample(backup_articles, num_backups)
 
-        all_articles = articles | backup_articles
-        return ArticleSet(articles=[all_articles[aid] for aid in sampled])
+        return ArticleSet(articles=sampled)
