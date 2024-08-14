abc1f78041146122386c96b988dfa03a95a8b90c
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-14 13:48:05 -0400

    use article IDs for filtering backups too

--- src/poprox_recommender/components/samplers/uniform.py
+++ src/poprox_recommender/components/samplers/uniform.py
@@ -9,22 +9,21 @@ class UniformSampler(Component):
         self.num_slots = num_slots
 
     def __call__(self, candidate: ArticleSet, backup: ArticleSet | None = None) -> ArticleSet:
-        backup_articles = list(
-            filter(
-                lambda article: article not in candidate.articles,
-                backup.articles if backup else [],
-            )
-        )
+        articles = {a.article_id: a for a in candidate.articles}
+
+        if backup.articles:
+            backup_articles = {a.article_id: a for a in backup.articles if a.article_id not in articles}
+        else:
+            backup_articles = []
 
         # we want to sample article IDs for performance
-        articles = {a.article_id: a for a in candidate.articles}
-        sampled = random.sample(articles.keys(), min(self.num_slots, len(candidate.articles)))
+
+        sampled = random.sample(list(articles.keys()), min(self.num_slots, len(candidate.articles)))
 
         if len(sampled) < self.num_slots and backup_articles:
-            backups = {b.article_id: b for b in backup_articles}
             # add backups to articles, but allow articles to override
-            articles = backups | articles
             num_backups = min(self.num_slots - len(sampled), len(backup_articles))
-            sampled += random.sample(backups.keys(), num_backups)
+            sampled += random.sample(list(backup_articles.keys()), num_backups)
 
-        return ArticleSet(articles=[articles[aid] for aid in sampled])
+        all_articles = articles | backup_articles
+        return ArticleSet(articles=[all_articles[aid] for aid in sampled])
