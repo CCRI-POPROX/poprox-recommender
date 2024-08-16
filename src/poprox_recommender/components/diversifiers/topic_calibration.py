cfef5927872d12120d5cbf5c2ea220b8d7477085
Karl Higley <khigley@umn.edu>
2024-08-16 08:10:16 -0400

    Apply topic calibration even without clicks (using onboarding topics) (#90)
    This creates article scores from onboarding topic selections when there
    are no clicks for the model to use. In that case, the normal article
    scorer returns `None`, which is a signal to downstream components to
    fill in the gap if they need to.
    
    Here the scores computation is very rough and just good enough to let
    topic calibration proceed as usual: the weights of onboarding topic
    selections that match article topics are summed and the resulting vector
    of article scores is divided by its max value to normalize the scores
    into the [0.0,1.0] range.
    
    Adding this to topic calibration was the easiest place to start, but I
    think there's an argument to be made that this could/should be a
    fallback scorer in all the pipelines with their various
    rerankers/diversifiers.

--- src/poprox_recommender/components/diversifiers/topic_calibration.py
+++ src/poprox_recommender/components/diversifiers/topic_calibration.py
@@ -19,21 +19,14 @@ class TopicCalibrator(Component):
         self.num_slots = num_slots
 
     def __call__(self, candidate_articles: ArticleSet, interest_profile: InterestProfile) -> ArticleSet:
-        if candidate_articles.scores is None:
-            return candidate_articles
+        normalized_topic_prefs = self.compute_topic_dist(interest_profile)
 
-        article_scores = th.sigmoid(th.tensor(candidate_articles.scores)).cpu().detach().numpy()
+        if candidate_articles.scores is not None:
+            article_scores = th.sigmoid(th.tensor(candidate_articles.scores))
+        else:
+            article_scores = th.zeros(len(candidate_articles.articles))
 
-        topic_preferences: dict[str, int] = defaultdict(int)
-
-        for interest in interest_profile.onboarding_topics:
-            topic_preferences[interest.entity_name] = max(interest.preference - 1, 0)
-
-        if interest_profile.click_topic_counts:
-            for topic, click_count in interest_profile.click_topic_counts.items():
-                topic_preferences[topic] += click_count
-
-        normalized_topic_prefs = normalized_topic_count(topic_preferences)
+        article_scores = article_scores.cpu().detach().numpy()
 
         article_indices = topic_calibration(
             article_scores,
@@ -45,6 +38,19 @@ class TopicCalibrator(Component):
 
         return ArticleSet(articles=[candidate_articles.articles[int(idx)] for idx in article_indices])
 
+    def compute_topic_dist(self, interest_profile):
+        topic_preferences: dict[str, int] = defaultdict(int)
+
+        for interest in interest_profile.onboarding_topics:
+            topic_preferences[interest.entity_name] = max(interest.preference - 1, 0)
+
+        if interest_profile.click_topic_counts:
+            for topic, click_count in interest_profile.click_topic_counts.items():
+                topic_preferences[topic] += click_count
+
+        normalized_topic_prefs = normalized_topic_count(topic_preferences)
+        return normalized_topic_prefs
+
 
 def topic_calibration(relevance_scores, articles, topic_preferences, theta, topk) -> list[Article]:
     # MR_i = \theta * reward_i - (1 - \theta)*C(S + i) # C is calibration
@@ -52,11 +58,8 @@ def topic_calibration(relevance_scores, articles, topic_preferences, theta, topk
 
     recommendations = []  # final recommendation (topk index)
     rec_topics = defaultdict(int)  # frequency distribution of topics of S
-    # first recommended item
-    recommendations.append(relevance_scores.argmax())
-    add_article_to_topics(rec_topics, articles[int(relevance_scores.argmax())])
 
-    for k in range(topk - 1):
+    for k in range(topk):
         candidate = None  # next item
         best_candidate_score = float("-inf")
 
