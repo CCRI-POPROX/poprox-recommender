051a7367c9156eaa2cfea30fb904ffcbef723067
Karl Higley <khigley@umn.edu>
2024-09-06 08:38:46 -0400

    Create a pipeline that uses `SoftmaxSampler` as the reranker
    Add a test of the pipeline

---
+++ tests/components/test_softmax_sampler.py
@@ -0,0 +1,40 @@
+import logging
+
+from poprox_concepts import ArticleSet
+from poprox_concepts.api.recommendations import RecommendationRequest
+from poprox_recommender.paths import project_root
+from poprox_recommender.recommenders import select_articles
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def test_request_with_softmax_sampler():
+    test_dir = project_root() / "tests"
+    req_f = test_dir / "request_data" / "medium_request.json"
+
+    req = RecommendationRequest.model_validate_json(req_f.read_text())
+
+    base_outputs = select_articles(
+        ArticleSet(articles=req.todays_articles),
+        ArticleSet(articles=req.past_articles),
+        req.interest_profile,
+    )
+    sampled_outputs = select_articles(
+        ArticleSet(articles=req.todays_articles),
+        ArticleSet(articles=req.past_articles),
+        req.interest_profile,
+        pipeline_params={"pipeline": "softmax"},
+    )
+
+    # do we get recommendations?
+    softmax_recs = sampled_outputs.default.articles
+    base_recs = base_outputs.default.articles
+    assert len(softmax_recs) > 0
+    assert len(base_recs) == len(softmax_recs)
+
+    base_article_ids = [article.article_id for article in base_recs]
+    sampled_article_ids = [article.article_id for article in softmax_recs]
+
+    # are the recommendation lists different?
+    assert base_article_ids != sampled_article_ids
