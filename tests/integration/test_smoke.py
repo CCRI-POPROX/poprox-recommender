a273e0e97b90f51c06a250fea96ee257767d7bf8
Michael Ekstrand <mdekstrand@drexel.edu>
2024-09-30 09:31:31 -0400

    fix smoke tests when no data

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -4,6 +4,8 @@ Test the POPROX API through direct call.
 
 import logging
 
+from pytest import skip
+
 from poprox_concepts import ArticleSet, Click
 from poprox_concepts.api.recommendations import RecommendationRequest
 from poprox_recommender.paths import project_root
@@ -18,11 +20,15 @@ def test_direct_basic_request():
     req = RecommendationRequest.model_validate_json(req_f.read_text())
 
     logger.info("generating recommendations")
-    outputs = select_articles(
-        ArticleSet(articles=req.todays_articles),
-        ArticleSet(articles=req.past_articles),
-        req.interest_profile,
-    )
+    try:
+        outputs = select_articles(
+            ArticleSet(articles=req.todays_articles),
+            ArticleSet(articles=req.past_articles),
+            req.interest_profile,
+        )
+    except FileNotFoundError:
+        # FIXME: when we have configuration files, separate "cannot load" from "cannot run"
+        skip("model data not available")
     # do we get recommendations?
     assert len(outputs.default.articles) > 0
 
@@ -36,10 +42,14 @@ def test_direct_basic_request_without_clicks():
 
     profile = req.interest_profile
     profile.click_history = []
-    outputs = select_articles(
-        ArticleSet(articles=req.todays_articles),
-        ArticleSet(articles=req.past_articles),
-        req.interest_profile,
-    )
+    try:
+        outputs = select_articles(
+            ArticleSet(articles=req.todays_articles),
+            ArticleSet(articles=req.past_articles),
+            req.interest_profile,
+        )
+    except FileNotFoundError:
+        # FIXME: when we have configuration files, separate "cannot load" from "cannot run"
+        skip("model data not available")
     # do we get recommendations?
     assert len(outputs.default.articles) > 0
