b6ee799631891bed6e60e235569de83ab3b7b173
Michael Ekstrand <mdekstrand@drexel.edu>
2024-09-30 14:25:47 -0400

    fix broken smoke tests

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -4,35 +4,35 @@ Test the POPROX API through direct call.
 
 import logging
 
-from pytest import skip
+from pytest import mark, skip
 
 from poprox_concepts import ArticleSet, Click
 from poprox_concepts.api.recommendations import RecommendationRequest
+from poprox_recommender.config import allow_data_test_failures
 from poprox_recommender.paths import project_root
-from poprox_recommender.recommenders import select_articles
+from poprox_recommender.recommenders import PipelineLoadError, select_articles
 
 logger = logging.getLogger(__name__)
 
 
+@mark.xfail(condition=allow_data_test_failures(), raises=PipelineLoadError, reason="data not pulled")
 def test_direct_basic_request():
     test_dir = project_root() / "tests"
     req_f = test_dir / "request_data" / "basic-request.json"
     req = RecommendationRequest.model_validate_json(req_f.read_text())
 
     logger.info("generating recommendations")
-    try:
-        outputs = select_articles(
-            ArticleSet(articles=req.todays_articles),
-            ArticleSet(articles=req.past_articles),
-            req.interest_profile,
-        )
-    except FileNotFoundError:
-        # FIXME: when we have configuration files, separate "cannot load" from "cannot run"
-        skip("model data not available")
+    outputs = select_articles(
+        ArticleSet(articles=req.todays_articles),
+        ArticleSet(articles=req.past_articles),
+        req.interest_profile,
+    )
+
     # do we get recommendations?
     assert len(outputs.default.articles) > 0
 
 
+@mark.xfail(condition=allow_data_test_failures(), raises=PipelineLoadError, reason="data not pulled")
 def test_direct_basic_request_without_clicks():
     test_dir = project_root() / "tests"
     req_f = test_dir / "request_data" / "basic-request.json"
@@ -42,14 +42,11 @@ def test_direct_basic_request_without_clicks():
 
     profile = req.interest_profile
     profile.click_history = []
-    try:
-        outputs = select_articles(
-            ArticleSet(articles=req.todays_articles),
-            ArticleSet(articles=req.past_articles),
-            req.interest_profile,
-        )
-    except FileNotFoundError:
-        # FIXME: when we have configuration files, separate "cannot load" from "cannot run"
-        skip("model data not available")
+    outputs = select_articles(
+        ArticleSet(articles=req.todays_articles),
+        ArticleSet(articles=req.past_articles),
+        req.interest_profile,
+    )
+
     # do we get recommendations?
     assert len(outputs.default.articles) > 0
