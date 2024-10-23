50a81f9b35a962d43bac54ce8fd35a5b68a4d632
Karl Higley <khigley@umn.edu>
2024-10-23 16:56:05 -0400

    Add a test that checks that deduplicate in `Fill` works correctly

--- src/poprox_recommender/components/joiners/fill.py
+++ src/poprox_recommender/components/joiners/fill.py
@@ -4,28 +4,32 @@ from poprox_recommender.lkpipeline.types import Lazy
 
 
 class Fill(Component):
-    def __init__(self, num_slots: int):
+    def __init__(self, num_slots: int, deduplicate: bool = True):
         self.num_slots = num_slots
+        self.deduplicate = deduplicate
 
     def __call__(self, candidates1: ArticleSet, candidates2: Lazy[ArticleSet]) -> ArticleSet:
         articles = candidates1.articles
 
-        # Track the articles by their article_id
-        existing_articles = {(article.article_id) for article in articles}
+        if self.deduplicate:
+            # Track the articles by their article_id
+            existing_articles = {(article.article_id) for article in articles}
 
-        # Add articles from candidates2 only if they are not duplicates
-        if len(articles) < self.num_slots:
-            new_articles = []
-            for article in candidates2.get().articles:
-                # Check if the article is a duplicate based on article_id
-                if (article.article_id) not in existing_articles:
-                    new_articles.append(article)
-                    existing_articles.add((article.article_id))  # Avoid future duplicates
-                # Stop if we have enough articles
-                if len(articles) + len(new_articles) >= self.num_slots:
-                    break
+            # Add articles from candidates2 only if they are not duplicates
+            if len(articles) < self.num_slots:
+                new_articles = []
+                for article in candidates2.get().articles:
+                    # Check if the article is a duplicate based on article_id
+                    if (article.article_id) not in existing_articles:
+                        new_articles.append(article)
+                        existing_articles.add((article.article_id))  # Avoid future duplicates
+                    # Stop if we have enough articles
+                    if len(articles) + len(new_articles) >= self.num_slots:
+                        break
 
-            articles = articles + new_articles
+                articles = articles + new_articles
+        else:
+            articles = articles + candidates2.get().articles
 
         # Return the resulting ArticleSet, limiting the size to num_slots
         return ArticleSet(articles=articles[: self.num_slots])
