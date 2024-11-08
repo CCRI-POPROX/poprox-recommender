22275bd99c8a4b0460c2c83c64fb0afaee38d188
Karl Higley <khigley@umn.edu>
2024-11-08 15:18:55 -0500

    Add a CloudFormation template and CF deploy script
    This sets up the IAM group and user required to deploy the recommender to an outside AWS account. I opted to keep the group (rather than collapse those permissions onto the user) in order to smooth the path for setting up additional users with the same permissions if desired.

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,18 @@ region=${region:-us-east-1}
 echo "ENV: $env"
 echo "Region: $region"
 
-# Download model artifacts
-dvc pull -R models
+# Check if a script name was provided as a command line argument
+if [ -z "$1" ]; then
+    echo "Error: Not sure whether to run cloudformation or serverless"
+    exit 1
+fi
 
-# Build container and deploy functions
-npx serverless deploy --stage "${env}" --region "${region}"
+if [ "$1" == "cf" ]; then
+    aws cloudformation deploy --template-file cloudformation.yml --stack-name poprox-research-recommender-"${env}" --region "${region}" --capabilities CAPABILITY_NAMED_IAM
+elif [ "$1" == "sls" ]; then
+    # Download model artifacts
+    dvc pull -R models
+
+    # Build container and deploy functions
+    npx serverless deploy --stage "${env}" --region "${region}"
+fi
