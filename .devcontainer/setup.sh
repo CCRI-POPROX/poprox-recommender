37db49b9b7a9fd937fec03b3bb9a3abdd9afa3cc
Michael Ekstrand <mdekstrand@drexel.edu>
2024-12-10 19:42:06 +0000

    node_modules mount + setup.sh bits

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -xeo pipefail
 
+# set up environment dirs
+sudo chown vscode:vscode .pixi node_modules
+
+# get pre-commit wired up and ready
 pre-commit install
 pre-commit install-hooks
+
+# install the development environment
 pixi install -e dev
+pixi run -e dev install-serverless
