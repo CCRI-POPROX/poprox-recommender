9fb5111efdfeaf67137f5ce6825a5d275a92f84d
Karl Higley <khigley@umn.edu>
2025-01-21 15:22:57 +0000

    Adjust the script to reorder it from most generic to most specific

--- local_req.py
+++ local_req.py
@@ -12,43 +12,51 @@ if __name__ == "__main__":
     with open("tests/request_data/no_clicks.json", "r") as req_file:
         raw_json = req_file.read()
 
-    event_1 = {
+    event_static = {
+        "body": raw_json,
+        "queryStringParameters": {"pipeline": "nrms-topics-static"},
+        "isBase64Encoded": False,
+    }
+    event_candidate = {
         "body": raw_json,
         "queryStringParameters": {"pipeline": "nrms-topics-candidate"},
         "isBase64Encoded": False,
     }
-    event_2 = {"body": raw_json, "queryStringParameters": {"pipeline": "nrms-topics-clicked"}, "isBase64Encoded": False}
-    event_3 = {"body": raw_json, "queryStringParameters": {"pipeline": "nrms-topics-static"}, "isBase64Encoded": False}
+    event_clicked = {
+        "body": raw_json,
+        "queryStringParameters": {"pipeline": "nrms-topics-clicked"},
+        "isBase64Encoded": False,
+    }
 
-    response_1 = generate_recs(event_1, {})
-    response_1 = RecommendationResponse.model_validate_json(response_1["body"])
+    response_static = generate_recs(event_static, {})
+    response_static = RecommendationResponse.model_validate_json(response_static["body"])
 
-    response_2 = generate_recs(event_2, {})
-    response_2 = RecommendationResponse.model_validate_json(response_2["body"])
+    response_candidate = generate_recs(event_candidate, {})
+    response_candidate = RecommendationResponse.model_validate_json(response_candidate["body"])
 
-    response_3 = generate_recs(event_3, {})
-    response_3 = RecommendationResponse.model_validate_json(response_3["body"])
+    response_clicked = generate_recs(event_clicked, {})
+    response_clicked = RecommendationResponse.model_validate_json(response_clicked["body"])
 
-    for profile_id, recs in response_1.recommendations.items():
+    for profile_id, recs in response_static.recommendations.items():
+        print("\n")
+        print(f"{event_static['queryStringParameters']['pipeline']}")
+
+        for idx, article in enumerate(recs):
+            article_topics = extract_general_topics(article)
+            print(f"{idx+1}. {article.headline} {article_topics}")
+
+    for profile_id, recs in response_candidate.recommendations.items():
         print("\n")
         print(f"Recs for {profile_id}:")
-        print(f"{event_1['queryStringParameters']['pipeline']}")
+        print(f"{event_candidate['queryStringParameters']['pipeline']}")
 
         for idx, article in enumerate(recs):
             article_topics = extract_general_topics(article)
             print(f"{idx+1}. {article.headline} {article_topics}")
 
-    for profile_id, recs in response_2.recommendations.items():
+    for profile_id, recs in response_clicked.recommendations.items():
         print("\n")
-        print(f"{event_2['queryStringParameters']['pipeline']}")
-
-        for idx, article in enumerate(recs):
-            article_topics = extract_general_topics(article)
-            print(f"{idx+1}. {article.headline} {article_topics}")
-
-    for profile_id, recs in response_3.recommendations.items():
-        print("\n")
-        print(f"{event_3['queryStringParameters']['pipeline']}")
+        print(f"{event_clicked['queryStringParameters']['pipeline']}")
 
         for idx, article in enumerate(recs):
             article_topics = extract_general_topics(article)
