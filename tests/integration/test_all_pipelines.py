4cc3d2d3ddf9edb6e3491a9d0c56ecec1d21e10a
Michael Ekstrand <mdekstrand@drexel.edu>
2025-01-24 23:17:26 +0000

    remove serverless listener

--- tests/integration/test_all_pipelines.py
+++ tests/integration/test_all_pipelines.py
@@ -8,7 +8,6 @@ import sys
 import warnings
 from threading import Condition, Lock, Thread
 
-from pexpect import EOF, spawn
 from pytest import fail, fixture, mark, skip
 
 from poprox_recommender.config import allow_data_test_failures
@@ -25,46 +24,6 @@ except Exception as e:
         skip("recommendation pipelines unavailable", allow_module_level=True)
     else:
         raise e
-
-
-@fixture(scope="module")
-def sl_listener():
-    """
-    Fixture that starts and stops serverless offline to test endpoint responses.
-    """
-
-    local = os.environ.get("POPROX_LOCAL_LAMBDA", None)
-    if local:
-        yield
-        return
-
-    thread = ServerlessBackground()
-    thread.start()
-    try:
-        with thread.lock:
-            if thread.ready.wait(15):
-                logger.info("ready for tests")
-                yield
-            else:
-                fail("serverless timed out")
-    finally:
-        thread.proc.sendintr()
-
-
-class ServerlessBackground(Thread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.lock = Lock()
-        self.ready = Condition(self.lock)
-
-    def run(self):
-        logger.info("starting serverless")
-        self.proc = spawn("npx serverless offline start", logfile=sys.stdout.buffer)
-        self.proc.expect(r"Server ready:")
-        logger.info("server ready")
-        with self.lock:
-            self.ready.notify_all()
-        self.proc.expect(EOF)
 
 
 @mark.parametrize("pipeline", PIPELINES)
