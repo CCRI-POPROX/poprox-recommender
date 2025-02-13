28cad1f203bfb3ecf8ac4ca03cb6255f4072999a
Sushobhan Parajuli <sp3886@gmail.com>
2025-02-12 23:54:08 -0500

    move `RequestGenerator`

--- src/poprox_recommender/testing.py
+++ src/poprox_recommender/testing.py
@@ -8,15 +8,20 @@ tests, regardless of their subdirectories.
 import json
 import logging
 import os
+import random
 import subprocess as sp
 from collections.abc import Generator
+from datetime import datetime, timedelta
 from signal import SIGINT
 from time import sleep
-from typing import Protocol
+from typing import List, Protocol
+from uuid import uuid4
 
 import requests
+from pydantic import ValidationError
 from pytest import fixture
 
+from poprox_concepts import AccountInterest, Click, InterestProfile
 from poprox_concepts.api.recommendations import RecommendationRequest, RecommendationResponse
 from poprox_recommender.data.mind import MindData
 
@@ -131,6 +136,81 @@ def auto_service() -> Generator[TestService, None, None]:
         yield DockerTestService(f"http://localhost:{port}/2015-03-31/functions/function/invocations")
 
 
+class RequestGenerator:
+    """
+    Class to generate recommendation request using click history,
+    onboarding topics, and candidate articles from MIND
+    """
+
+    def __init__(self, mind_data: MindData):
+        self.mind_data = mind_data
+        self.profile_id = uuid4()
+        self.candidate_articles = list()
+        self.past_articles = list()
+        self.added_topics = list()
+        self.clicks = list()
+
+    def set_num_recs(self, num_recs: int):
+        self.num_recs = num_recs
+
+    def add_clicks(self, num_clicks: int, num_days: int | None = None):
+        all_articles = list(self.mind_data.news_df.index)
+
+        if num_days:
+            start_date = datetime.now() - timedelta(days=num_days - 1)
+            timestamps = [start_date + timedelta(days=random.randint(0, num_days - 1)) for _ in range(num_clicks)]
+            random.shuffle(timestamps)
+        else:
+            timestamps = [datetime.now()] * num_clicks
+        # generate click history
+        self.clicks = [
+            Click(
+                article_id=self.mind_data.news_uuid_for_id(random.choice(all_articles)),
+                newsletter_id=uuid4(),
+                timestamp=timestamps[i],
+            )
+            for i in range(num_clicks)
+        ]
+
+        self.past_articles = [self.mind_data.lookup_article(uuid=click.article_id) for click in self.clicks]
+
+    def add_topics(self, topics: List[str]):
+        self.added_topics = [
+            AccountInterest(
+                account_id=self.profile_id,
+                entity_id=uuid4(),
+                entity_name=topic,
+                preference=random.randint(1, 5),
+                frequency=None,
+            )
+            for topic in topics
+        ]
+
+    def add_candidates(self, num_candidates):
+        all_articles = list(self.mind_data.news_df.index)
+        selected_candidates = random.sample(all_articles, num_candidates)
+
+        self.candidate_articles = [self.mind_data.lookup_article(id=article_id) for article_id in selected_candidates]
+
+    def get_request(self) -> RecommendationRequest:
+        interest_profile = InterestProfile(
+            profile_id=self.profile_id,
+            click_history=self.clicks,
+            onboarding_topics=self.added_topics,
+        )
+
+        try:
+            request = RecommendationRequest(
+                past_articles=self.past_articles,
+                todays_articles=self.candidate_articles,
+                interest_profile=interest_profile,
+                num_recs=self.num_recs,
+            )
+            return request
+        except ValidationError as e:
+            raise ValueError(f"Generated request is invalid: {e}")
+
+
 @fixture(scope="session")
 def mind_data():
-    yield MindData
+    yield MindData()
--- tests/web_service/test_basic_request.py
+++ tests/web_service/test_basic_request.py
@@ -4,6 +4,7 @@ Test basic request by initializing test data.
 
 import logging
 import warnings
+from uuid import uuid4
 
 from pydantic import ValidationError
 from pytest import mark, skip
@@ -11,9 +12,8 @@ from pytest import mark, skip
 from poprox_concepts.api.recommendations import RecommendationRequest, RecommendationResponse
 from poprox_recommender.config import allow_data_test_failures
 from poprox_recommender.recommenders import recommendation_pipelines
-from poprox_recommender.request_generator import RequestGenerator
+from poprox_recommender.testing import RequestGenerator, mind_data
 from poprox_recommender.testing import auto_service as service
-from poprox_recommender.testing import mind_data
 
 logger = logging.getLogger(__name__)
 try:
@@ -26,13 +26,13 @@ except Exception as e:
         raise e
 
 
-@mark.docker
 @mark.parametrize("pipeline", PIPELINES)
 def test_basic_request(service, mind_data, pipeline):  # noqa: F811
     """
     Initialize request data
     """
-    request_generator = RequestGenerator(mind_data())
+    request_generator = RequestGenerator(mind_data)
+    request_generator.profile_id = uuid4()
     request_generator.add_candidates(100)
     request_generator.add_clicks(num_clicks=37, num_days=7)
     request_generator.add_topics(
