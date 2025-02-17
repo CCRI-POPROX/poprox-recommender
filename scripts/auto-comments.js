e3b6e4eb53a352becc95fcc82a6f7ed044a88adc
Michael Ekstrand <mdekstrand@drexel.edu>
2025-02-17 12:12:03 -0500

    post comment instead of failing when pipeline is out of date

---
+++ scripts/auto-comments.js
@@ -0,0 +1,37 @@
+const fs = require("fs/promises");
+
+async function postDvcStatus() {
+    let body = await fs.readFile("dvc-status.log", { encoding: "utf-8" });
+
+    let comments = octokit.rest.issues.listComments({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+    });
+    let comment_id = null;
+    for (let c of comments) {
+        if (c.body.match(/Creator:\s+check-dvc-status/)) {
+            comment_id = c.id;
+            break;
+        }
+    }
+    if (comment_id) {
+        octokit.rest.issues.updateComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            comment_id,
+            body,
+        });
+    } else {
+        octokit.rest.issues.createComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: context.issue.number,
+            body,
+        });
+    }
+}
+
+module.exports = {
+    postDvcStatus,
+};
