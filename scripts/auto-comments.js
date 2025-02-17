778b621d2af3326abcff554a2f8ec0fac230034d
Michael Ekstrand <mdekstrand@drexel.edu>
2025-02-18 07:28:29 +1100

    Post (or update) a comment instead of failing when pipeline is out of date (#161)
    This modifies the “is the pipeline up-to-date?” logic to post a comment
    back to the PR instead of failing the build.
    
    The job minimizes all prior status comments as "outdated" to reduce PR
    clutter.

---
+++ scripts/auto-comments.js
@@ -0,0 +1,42 @@
+const fs = require("fs/promises");
+
+async function postDvcStatus({ github, context }) {
+    let body = await fs.readFile("dvc-status.log", { encoding: "utf-8" });
+    console.log("posting issue comment");
+
+    let comments = await github.rest.issues.listComments({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+    });
+    if (comments.status != 200) {
+        throw new Error(`HTTP error fetching comments (${comments.status})`);
+    }
+    let comment_id = null;
+    // minimize all previous comments
+    for (let c of comments.data) {
+        if (c.body.match(/Creator:\s+check-dvc-status/)) {
+            await github.graphql(`
+                mutation MinimizeComment {
+                    minimizeComment(input: {subjectId:"${c.node_id}",classifier:OUTDATED}) {
+                        minimizedComment {
+                            minimizedReason
+                        }
+                    }
+                }
+            `);
+        }
+    }
+
+    // add a new comment
+    await github.rest.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+        body,
+    });
+}
+
+module.exports = {
+    postDvcStatus,
+};
