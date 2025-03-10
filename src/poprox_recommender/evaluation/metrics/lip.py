906bdd12a638eee545f325751be27be5d7581114
Aishwarya Satwani <aishwarya.satwani@colorado.edu>
2025-03-10 20:17:14 +0000

    lip and kcoverage tested

--- src/poprox_recommender/evaluation/metrics/lip.py
+++ src/poprox_recommender/evaluation/metrics/lip.py
@@ -2,9 +2,25 @@ from poprox_concepts.domain import CandidateSet
 
 
 def least_item_promoted(reference_article_set: CandidateSet, reranked_article_set: CandidateSet, k: int = 10) -> float:
+    """
+    ValueError: Article(article_id=UUID('6b6b748e-0212-5b29-bf90-3b97f6da540d'), headline='', subhead=None, body=None,
+    url=None,preview_image_id=None, mentions=[], source=None, external_id=None, raw_data=None, images=None,
+    published_at=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=datetime.timezone.utc),created_at=None) is not in list
+    """
     lip_rank = 0
     for item in reference_article_set.articles:
-        rank_item = reranked_article_set.index(item) + 1
+        try:
+            rank_item = (
+                next(
+                    i
+                    for i, article in enumerate(reranked_article_set.articles[:k])
+                    if article.article_id == item.article_id
+                )
+                + 1
+            )
+        except StopIteration:
+            rank_item = k + 1
+        rank_item = reranked_article_set.articles.index(item) + 1
         if rank_item > k:
             if rank_item > lip_rank:
                 lip_rank = rank_item
