e867ababf797b199a2758bf8f8b31e53f9f02a86
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-12 15:03:51 -0400

    Refactor pipelines into separate configurations + cache (#169)
    This is the Pipeline Config Refactor, giving us configuration "files"
    and caching.
    
    - Move each pipeline configuration into a separate file (with a `common`
    file for standard elements of the configuration).
    - Rewrite the pipeline load logic to scan the avialable files, instead
    of using a hard-coded list, and load pipelines from there.
    - Allow only the desired pipeline to be loaded, instead of loading all.
    - Use a `PipelineCache` to automatically share common componens across
    all loaded pipelines.
    - Remove the concept of multiple pipeline names — give all pipelines one
    name, the name of their module. Right now those names are unchanged from
    original, I would like to rename for consistency and clarity.
    
    With pipelines in separate files, we will be able to add (in another PR,
    probaly) logic to generate and measure separately, so we only need to
    re-run evaluation for the pipeline we add or change instead of
    re-running _everything_.

---
+++ src/poprox_recommender/recommenders/load.py
@@ -0,0 +1,86 @@
+"""
+Functions and logic for loading recommender pipeline configurations.
+"""
+
+# pyright: basic
+from importlib import import_module
+from importlib.metadata import version
+from pathlib import Path
+
+from lenskit.pipeline import Pipeline, PipelineBuilder, PipelineCache
+from structlog.stdlib import get_logger
+
+from poprox_recommender.config import default_device
+
+logger = get_logger(__name__)
+
+
+_cached_pipelines: dict[str, Pipeline] = {}
+_component_cache = PipelineCache()
+
+
+class PipelineLoadError(Exception):
+    """
+    Exception raised when a pipeline cannot be loaded or instantiated, to
+    separate those errors from errors running the pipeline.
+    """
+
+
+def discover_pipelines() -> list[str]:
+    """
+    Discover the list of available pipeline configuration names.
+    """
+    cfg_dir = Path(__file__).parent / "configurations"
+    names = [p.stem for p in cfg_dir.glob("*.py") if not p.name.startswith("_")]
+    logger.debug("scanned pipeline configurations", path=str(cfg_dir), count=len(names))
+    return names
+
+
+def get_pipeline_builder(name: str, device: str | None = None, num_slots: int = 10) -> PipelineBuilder:
+    """
+    Get a pipeline builder by name.
+    """
+    if device is None:
+        device = default_device()
+
+    # get a Python-compatible name
+    norm_name = name.replace("-", "_")
+    logger.debug("configuring pipeline", name=norm_name, device=device, num_slots=num_slots)
+    # load the module
+    mod_name = f"poprox_recommender.recommenders.configurations.{norm_name}"
+    pipe_mod = import_module(mod_name)
+
+    pipe_ver = getattr(mod_name, "VERSION", None)
+    if pipe_ver is None:
+        pipe_ver = version("poprox-recommender")
+
+    builder = PipelineBuilder(norm_name, pipe_ver)
+    pipe_mod.configure(builder, num_slots=num_slots, device=device)
+    return builder
+
+
+def get_pipeline(name: str, device: str | None = None, num_slots: int = 10) -> Pipeline:
+    """
+    Get a built pipeline by name.
+    """
+    pipeline = _cached_pipelines.get(name, None)
+    if pipeline is None:
+        try:
+            builder = get_pipeline_builder(name, device, num_slots)
+            pipeline = builder.build(_component_cache)
+        except ImportError as e:
+            logger.error("error importing pipeline", name=name, device=device, num_slots=num_slots, exc_info=e)
+            raise PipelineLoadError(f"could not import pipeline {name}")
+        except Exception as e:
+            logger.error("failed to load pipeline", name=name, device=device, num_slots=num_slots, exc_info=e)
+            raise PipelineLoadError(f"could not load pipeline {name}")
+        _cached_pipelines[name] = pipeline
+
+    return pipeline
+
+
+def load_all_pipelines(device: str | None = None, num_slots: int = 10) -> dict[str, Pipeline]:
+    logger.debug("loading all pipelines")
+
+    names = discover_pipelines()
+    return {n: get_pipeline(n, device, num_slots) for n in names}
--- tests/web_service/test_heavy_interaction_history.py
+++ tests/web_service/test_heavy_interaction_history.py
@@ -10,22 +10,14 @@ from pytest import mark, skip
 
 from poprox_concepts.api.recommendations import RecommendationRequest, RecommendationResponse
 from poprox_recommender.config import allow_data_test_failures
-from poprox_recommender.recommenders import recommendation_pipelines
+from poprox_recommender.recommenders import discover_pipelines
 from poprox_recommender.testing import RequestGenerator, mind_data
 from poprox_recommender.testing import auto_service as service
 
 logger = logging.getLogger(__name__)
-try:
-    PIPELINES = recommendation_pipelines().keys()
-except Exception as e:
-    warnings.warn("failed to load models, did you run `dvc pull`?")
-    if allow_data_test_failures():
-        skip("recommendation pipelines unavailable", allow_module_level=True)
-    else:
-        raise e
 
 
-@mark.parametrize("pipeline", PIPELINES)
+@mark.parametrize("pipeline", discover_pipelines())
 def test_heavy_interaction_history(service, mind_data, pipeline):  # noqa: F811
     """
     Initialize request data
