8b99f8e019e83469b7ec59cd15dffd220daf1752
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-12 11:14:38 -0400

    remove common and re-duplicate configurations

--- src/poprox_recommender/recommenders/configurations/nrms_rrf_static_user.py
+++ src/poprox_recommender/recommenders/configurations/nrms_rrf_static_user.py
@@ -2,48 +2,81 @@
 
 from lenskit.pipeline import PipelineBuilder
 
+from poprox_concepts import CandidateSet, InterestProfile
 from poprox_recommender.components.embedders import NRMSArticleEmbedder
-from poprox_recommender.components.embedders.topic_wise_user import UserOnboardingEmbedder
-from poprox_recommender.components.embedders.user import NRMSUserEmbedder
-from poprox_recommender.components.joiners import ReciprocalRankFusion
+from poprox_recommender.components.embedders.article import NRMSArticleEmbedderConfig
+from poprox_recommender.components.embedders.topic_wise_user import UserOnboardingConfig, UserOnboardingEmbedder
+from poprox_recommender.components.embedders.user import NRMSUserEmbedder, NRMSUserEmbedderConfig
+from poprox_recommender.components.filters.topic import TopicFilter
+from poprox_recommender.components.joiners.fill import Fill
+from poprox_recommender.components.joiners.rrf import ReciprocalRankFusion
 from poprox_recommender.components.rankers.topk import TopkRanker
-from poprox_recommender.components.scorers import ArticleScorer
+from poprox_recommender.components.samplers.uniform import UniformSampler
+from poprox_recommender.components.scorers.article import ArticleScorer
 from poprox_recommender.paths import model_file_path
-
-from .. import common
 
 
 def configure(builder: PipelineBuilder, num_slots: int, device: str):
-    common.add_inputs(builder)
-    common.add_article_embedder(
-        builder, NRMSArticleEmbedder, model_path=model_file_path("nrms-mind/news_encoder.safetensors"), device=device
+    # standard practice is to put these calls in this order, to reuse logic
+    # Define pipeline inputs
+    i_candidates = builder.create_input("candidate", CandidateSet)
+    i_clicked = builder.create_input("clicked", CandidateSet)
+    i_profile = builder.create_input("profile", InterestProfile)
+
+    # Embed candidate and clicked articles
+    ae_config = NRMSArticleEmbedderConfig(
+        model_path=model_file_path("nrms-mind/news_encoder.safetensors"), device=device
     )
-    common.add_user_embedder(
-        builder,
+    e_candidates = builder.add_component("candidate-embedder", NRMSArticleEmbedder, ae_config, article_set=i_candidates)
+    e_clicked = builder.add_component(
+        "history-NRMSArticleEmbedder", NRMSArticleEmbedder, ae_config, article_set=i_clicked
+    )
+
+    # Embed the user (historical clicks)
+    ue_config = NRMSUserEmbedderConfig(model_path=model_file_path("nrms-mind/user_encoder.safetensors"), device=device)
+    e_user = builder.add_component(
+        "user-embedder",
         NRMSUserEmbedder,
+        ue_config,
+        candidate_articles=e_candidates,
+        clicked_articles=e_clicked,
+        interest_profile=i_profile,
+    )
+
+    # Embed the user (topics)
+    ue_config2 = UserOnboardingConfig(
         model_path=model_file_path("nrms-mind/user_encoder.safetensors"),
         device=device,
+        embedding_source="static",
+        topic_embedding="avg",
     )
-    common.add_scorer(builder)
-    o_rank_1 = common.add_rankers(builder, num_slots=num_slots, recommender=False)
-
-    # Second user embedding strategy
-    e_user_2 = builder.add_component(
+    e_user2 = builder.add_component(
         "user-embedder2",
         UserOnboardingEmbedder,
-        {
-            "model_path": model_file_path("nrms-mind/user_encoder.safetensors"),
-            "device": device,
-            "embedding_source": "static",
-            "topic_embedding": "avg",
-        },
-        candidate_articles=builder.node("candidate"),
-        clicked_articles=builder.node("history-embedder"),
-        interest_profile=builder.node("profile"),
+        ue_config2,
+        candidate_articles=e_candidates,
+        clicked_articles=e_clicked,
+        interest_profile=i_profile,
     )
-    o_scored_2 = builder.add_component(
-        "scorer2", ArticleScorer, candidate_articles=builder.node("candidate-embedder"), interest_profile=e_user_2
-    )
-    o_rank_2 = builder.add_component("ranker2", TopkRanker, {"num_slots": num_slots}, candidate_articles=o_scored_2)
 
-    builder.add_component("recommender", ReciprocalRankFusion, {"num_slots": num_slots}, recs1=o_rank_1, recs2=o_rank_2)
+    # Score and rank articles (history)
+    n_scorer = builder.add_component("scorer", ArticleScorer, candidate_articles=e_candidates, interest_profile=e_user)
+    n_ranker = builder.add_component("ranker", TopkRanker, {"num_slots": num_slots}, candidate_articles=n_scorer)
+
+    # Score and rank articles (topics)
+    n_scored2 = builder.add_component(
+        "scorer2", ArticleScorer, candidate_articles=builder.node("candidate-embedder"), interest_profile=e_user2
+    )
+    n_ranker2 = builder.add_component("ranker2", TopkRanker, {"num_slots": num_slots}, candidate_articles=n_scored2)
+
+    # Fallback: sample from user topic interests
+    n_topic_filter = builder.add_component(
+        "topic-filter", TopicFilter, candidate=i_candidates, interest_profile=i_profile
+    )
+    n_sampler = builder.add_component("sampler", UniformSampler, candidates1=n_topic_filter, candidates2=i_candidates)
+
+    # Combine click and topic scoring
+    builder.add_component(
+        "recommender", ReciprocalRankFusion, {"num_slots": num_slots}, recs1=n_ranker, recs2=n_ranker2
+    )
+    builder.add_component("recommender", Fill, {"num_slots": num_slots}, recs1=n_ranker, recs2=n_sampler)
