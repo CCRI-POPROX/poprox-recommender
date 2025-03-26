db848544a649a3cc82277326598af8af158cae2f
sophiasun0515 <sophiasun0515@gmail.com>
2025-03-26 15:06:05 +0000

    add min_click_num support for response construction

--- tests/generate_test_request.py
+++ tests/generate_test_request.py
@@ -3,10 +3,11 @@ With exported dataset, this script can help generate test request body for recom
 The data export dataset needs to be placed in side poprox-recommender/data/POPROX folder, with timestamp removed
 
 Usage:
-    generate_test_request.py [--account_id ID] [--output_file OUTPUT]
+    generate_test_request.py [--account_id ID] [--min_click_num NUM] [--output_file OUTPUT]
 
 Options:
     --account_id ID      Specific user account id to process request data
+    --min_click_num NUM      Get a random user with at least min_click_num clicks
     --output_file OUTPUT Path to the output file
 """
 
@@ -35,6 +36,17 @@ def get_single_request() -> str:
                     req,
                     exclude={"candidates": excluded_fields, "interacted": excluded_fields, "protocol_version": True},
                 )
+                break
+    elif options["--min_click_num"]:
+        min_click = options["--min_click_num"]
+        for req in requests:
+            print(len(req.interest_profile.click_history))
+            if req.interest_profile.click_history and len(req.interest_profile.click_history) >= int(min_click):
+                request_body = RecommendationRequestV2.model_dump_json(
+                    req,
+                    exclude={"candidates": excluded_fields, "interacted": excluded_fields, "protocol_version": True},
+                )
+                break
     else:
         random_index = random.randint(0, len(requests) - 1)
         request_body = RecommendationRequestV2.model_dump_json(
