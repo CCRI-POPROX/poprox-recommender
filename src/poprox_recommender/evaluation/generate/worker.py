5e9becd779eb9c7199ccf00360ce744a4f432eea
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-17 07:40:36 -0400

    Refactor generation and evaluation to use Ray (#183)
    This refactors our batch-inference and evaluation code to use Ray
    instead of ipyparallel; even the ipyparallel people suggest Ray these
    days. It also tweaks our eval dependency setup to use a UV group instead
    of extra, making it easier to make sure we have the parallelism bits
    installed.
    
    Key changes:
    
    - Batch-inference and evaluation now use Ray to parallelize instead of
    ipyparallel.
    - Parallelism now uses LensKit's parallel processing configuration, so
    we configure with `LK_NUM_PROCS` instead of `POPROX_NUM_PROCS`. LensKit
    also automatically sets up Ray workers to limit per-process parallelism
    to avoid oversubscription.
    - Remove a bunch of parallel logging configuration that is now handled
    by LensKit's Ray support.
    - Make the embedding writer its own Ray actor, so we don't need to write
    to temporary files and then deduplicate.
    
    This is Part 1 of a two-part refactor of offline evaluation. Part 2 will
    separate pipelines into their own evaluation jobs, so we don't have to
    re-evaluate the entire set of pipelines just because one changed.
    
    Adding Ray as a dependency and using it for our batch inference also
    begins paving the way for later using ray-tune to do hyperparameter
    tuning.

--- src/poprox_recommender/evaluation/generate/worker.py
+++ src/poprox_recommender/evaluation/generate/worker.py
@@ -1,14 +1,14 @@
 import itertools as it
-import logging
 import multiprocessing as mp
 from uuid import UUID
 
-import ipyparallel as ipp
 import numpy as np
 import pandas as pd
-import pyarrow as pa
-from lenskit.logging import item_progress
-from lenskit.logging.worker import WorkerContext, WorkerLogConfig
+import ray
+import torch
+from lenskit.logging import get_logger, item_progress
+from lenskit.parallel import get_parallel_config
+from lenskit.parallel.ray import init_cluster
 from lenskit.pipeline import Pipeline
 from lenskit.pipeline.state import PipelineState
 from lenskit.util import Stopwatch
@@ -17,92 +17,109 @@ from poprox_concepts.api.recommendations import RecommendationRequest
 from poprox_concepts.domain import CandidateSet, RecommendationList
 from poprox_recommender.config import default_device
 from poprox_recommender.data.mind import TEST_REC_COUNT
-from poprox_recommender.evaluation.generate.outputs import RecOutputs
+from poprox_recommender.evaluation.generate.outputs import EmbeddingWriter, RecOutputs
 from poprox_recommender.recommenders import load_all_pipelines
 
-logger = logging.getLogger(__name__)
+logger = get_logger(__name__)
 
+BATCH_SIZE = 10
 STAGES = ["final", "ranked", "reranked"]
 
-# globals used for workers
-_pipelines: dict[str, Pipeline]
-_worker_out: RecOutputs
-_worker_log: WorkerContext | None = None
-_emb_seen: set[UUID]
+
+class RecGenerator:
+    """
+    Generate recommendations. Can be used as a Ray actor.
+    """
+
+    pipelines: dict[str, Pipeline]
+    worker_out: RecOutputs
+    emb_seen: set[UUID]
+    emb_writer: EmbeddingWriter
+
+    def __init__(self, outs: RecOutputs, emb_writer: EmbeddingWriter):
+        proc = mp.current_process()
+        self.pipelines = load_all_pipelines(device=default_device())
+        self.worker_out = outs
+        self.worker_out.open(proc.pid)
+        self.emb_seen = set()
+        self.emb_writer = emb_writer
+
+    def generate(self, request: RecommendationRequest) -> UUID | None:
+        log = logger.bind(profile_id=str(request.interest_profile.profile_id))
+        log.debug("beginning recommendation")
+        if request.num_recs != TEST_REC_COUNT:
+            log.warning(
+                "request for %s had unexpected recommendation count %d",
+                request.interest_profile.profile_id,
+                request.num_recs,
+            )
+
+        pipe_names = list(self.pipelines.keys())
+        inputs = {
+            "candidate": CandidateSet(articles=request.todays_articles),
+            "clicked": CandidateSet(articles=request.past_articles),
+            "profile": request.interest_profile,
+        }
+
+        for name, pipe in self.pipelines.items():
+            try:
+                outputs = pipe.run_all(**inputs)
+            except Exception as e:
+                logger.error("error recommending for profile %s: %s", request.interest_profile.profile_id, e)
+                raise e
+
+            rec_df, embeddings = extract_recs(name, request, outputs)
+            rec_df["recommender"] = pd.Categorical(rec_df["recommender"], categories=pipe_names)
+            rec_df["stage"] = pd.Categorical(rec_df["stage"].astype("category"), categories=STAGES)
+            self.worker_out.rec_writer.write_frame(rec_df)
+
+            # find any embeddings we haven't yet written (reduces overhead)
+            # the writer will also deduplicate between workers.
+            emb_to_write = {aid: emb for (aid, emb) in embeddings.items() if aid not in self.emb_seen}
+            # call remote if we have an actor
+            if hasattr(self.emb_writer.write_embeddings, "remote"):
+                ray.get(self.emb_writer.write_embeddings.remote(emb_to_write))
+            else:
+                self.emb_writer.write_embeddings(emb_to_write)
+            self.emb_seen |= embeddings.keys()
+
+        # just return the ID to indicate success
+        return request.interest_profile.profile_id
+
+    def generate_batch(self, batch: list[RecommendationRequest]) -> list[UUID | None]:
+        return [self.generate(req) for req in batch]
+
+    def finish(self):
+        self.worker_out.close()
+
+        try:
+            import resource
+
+            return resource.getrusage(resource.RUSAGE_SELF)
+        except ImportError:
+            return None
 
 
-def _init_worker(outs: RecOutputs, logging: WorkerLogConfig | None = None):
-    global _worker_out, _emb_seen, _pipelines, _worker_log
-    proc = mp.current_process()
-    _worker_out = outs
-    _emb_seen = set()
-    if logging is not None:
-        _worker_log = WorkerContext(logging)
-        _worker_log.start()
-
-    _worker_out.open(proc.pid)
-
-    _pipelines = load_all_pipelines(device=default_device())
-
-
-def _finish_worker():
-    global _worker_log
-    logger.info("closing output files")
-    _worker_out.close()
-    if _worker_log is not None:
-        _worker_log.shutdown()
-        _worker_log = None
-
-    try:
-        import resource
-
-        return resource.getrusage(resource.RUSAGE_SELF)
-    except ImportError:
-        return None
-
-
-def _generate_for_request(request: RecommendationRequest) -> UUID | None:
-    global _emb_seen
-
-    logger.debug("recommending for profile %s", request.interest_profile.profile_id)
-    if request.num_recs != TEST_REC_COUNT:
-        logger.warning(
-            "request for %s had unexpected recommendation count %d",
-            request.interest_profile.profile_id,
-            request.num_recs,
+def dynamic_remote(actor):
+    pc = get_parallel_config()
+    if torch.cuda.is_available():
+        _cuda_props = torch.cuda.get_device_properties()
+        # Let's take a wild guess that 20 MP units are enough per worker, so a
+        # 80-MP A40 can theoretically run 4 workers.  Even though we limit
+        # parallelism through an actor pool, if we do not request GPUs, Ray will
+        # keep us from accessing them.
+        remote = ray.remote(
+            num_cpus=pc.backend_threads,
+            num_gpus=20 / _cuda_props.multi_processor_count,
+        )
+    else:
+        # if we don't have CUDA, don't request GPU
+        remote = ray.remote(
+            num_cpus=pc.total_threads,
+            num_gpus=0,
         )
 
-    pipe_names = list(_pipelines.keys())
-    inputs = {
-        "candidate": CandidateSet(articles=request.todays_articles),
-        "clicked": CandidateSet(articles=request.past_articles),
-        "profile": request.interest_profile,
-    }
-
-    for name, pipe in _pipelines.items():
-        try:
-            outputs = pipe.run_all(**inputs)
-        except Exception as e:
-            logger.error("error recommending for profile %s: %s", request.interest_profile.profile_id, e)
-            raise e
-
-        rec_df, embeddings = extract_recs(name, request, outputs)
-        rec_df["recommender"] = pd.Categorical(rec_df["recommender"], categories=pipe_names)
-        rec_df["stage"] = pd.Categorical(rec_df["stage"].astype("category"), categories=STAGES)
-        _worker_out.rec_writer.write_frame(rec_df)
-
-        # find any embeddings not yet written
-        emb_rows = [
-            {"article_id": str(aid), "embedding": emb} for (aid, emb) in embeddings.items() if aid not in _emb_seen
-        ]
-        _emb_seen |= embeddings.keys()
-        if emb_rows:
-            # directly use pyarrow to avoid DF overhead, small but easy to avoid here
-            emb_tbl = pa.Table.from_pylist(emb_rows)
-            _worker_out.emb_writer.write_frame(emb_tbl)
-
-    # just return the ID to indicate success
-    return request.interest_profile.profile_id
+    return remote(actor)
 
 
 def extract_recs(
@@ -172,7 +189,7 @@ def extract_recs(
     return output_df, embeddings
 
 
-def generate_profile_recs(dataset: str, outs: RecOutputs, n_profiles: int | None = None, n_jobs: int = 1):
+def generate_profile_recs(dataset: str, outs: RecOutputs, n_profiles: int | None = None):
     logger.info("generating recommendations")
 
     profile_iter = dataset.iter_profiles()
@@ -185,32 +202,37 @@ def generate_profile_recs(dataset: str, outs: RecOutputs, n_profiles: int | None
 
     timer = Stopwatch()
     with item_progress("recommend", total=n_profiles) as pb:
-        if n_jobs > 1:
-            logger.info("starting evaluation with %d workers", n_jobs)
-            with ipp.Cluster(n=n_jobs) as client:
-                dv = client.direct_view()
-                logger.debug("initializing workers")
-                dv.apply_sync(_init_worker, outs, WorkerLogConfig.current())
+        pc = get_parallel_config()
+        if pc.processes > 1:
+            logger.info("starting evaluation with %d workers", pc.processes)
+            init_cluster(global_logging=True)
 
-                logger.debug("dispatching jobs")
-                lbv = client.load_balanced_view()
-                for uid in lbv.imap(_generate_for_request, profile_iter, max_outstanding=n_jobs * 5, ordered=False):
-                    logger.debug("finished measuring %s", uid)
-                    pb.update()
+            emb_out = ray.remote(EmbeddingWriter).remote(outs)
 
-                logger.info("generation finished, closing outputs")
-                rusage = dv.apply_sync(_finish_worker)
+            gen_actor = dynamic_remote(RecGenerator)
+            actors = [gen_actor.remote(outs, emb_out) for _i in range(pc.processes)]
+            pool = ray.util.ActorPool(actors)
+            batches = it.batched(profile_iter, BATCH_SIZE)
+
+            for rbatch in pool.map_unordered(lambda a, b: a.generate_batch.remote(b), batches):
+                pb.update(len(rbatch))
+
+            logger.info("closing actors")
+            rusage = [ray.get(actor.finish.remote()) for actor in actors]
+            ray.get(emb_out.close.remote())
 
         else:
             logger.info("starting serial evaluation")
             # directly call things in-process
-            _init_worker(outs)
+            emb_out = EmbeddingWriter(outs)
+            gen = RecGenerator(outs, emb_out)
 
             for request in profile_iter:
-                _generate_for_request(request)
+                gen.generate(request)
                 pb.update()
 
-            _finish_worker()
+            gen.finish()
+            emb_out.close()
             rusage = None
 
     timer.stop()
