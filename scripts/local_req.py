bbbb9fc9fda710def9cab23bb1ae35396047c5ef
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-05-21 18:22:28 +0000

    update local req for dummy data

--- scripts/local_req.py
+++ scripts/local_req.py
@@ -9,20 +9,25 @@ warnings.filterwarnings("ignore")
 
 
 if __name__ == "__main__":
-    with open(project_root() / "tests/request_data/onboarding.json", "r") as req_file:
-        raw_json = req_file.read()
-        req = RecommendationRequestV2.model_validate_json(raw_json)
+    project_root_path = project_root()
+    profile_paths = (project_root_path / "tests" / "request_data" / "profiles").glob("*.json")
+    for profile_path in profile_paths:
+        print(f"Loading profile from {profile_path}")
 
-    event_llm = {
-        "body": raw_json,
-        "queryStringParameters": {"pipeline": "llm_rank_rewrite"},
-        "isBase64Encoded": False,
-    }
+        with open(profile_path, "r") as profile_file:
+            raw_json = profile_file.read()
+            req = RecommendationRequestV2.model_validate_json(raw_json)
 
-    response_llm = root(req.model_dump(), pipeline="llm_rank_rewrite")
-    response_llm = RecommendationResponseV2.model_validate(response_llm)
+        event_profile = {
+            "body": raw_json,
+            "queryStringParameters": {"pipeline": "llm_rank_rewrite"},
+            "isBase64Encoded": False,
+        }
 
-    print("\n")
-    print(f"{event_llm['queryStringParameters']['pipeline']}")
-    for idx, article in enumerate(response_llm.recommendations.articles):
-        print(f"{idx + 1}. {article.headline}")
+        response_llm = root(req.model_dump(), pipeline="llm_rank_rewrite")
+        response_llm = RecommendationResponseV2.model_validate(response_llm)
+
+        print("\n")
+        print(f"{event_profile['queryStringParameters']['pipeline']}")
+        for idx, article in enumerate(response_llm.recommendations.articles):
+            print(f"{idx + 1}. {article.headline}")
