c7c2ba596a7b6e9222b434bdc46bfe2a57940142
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-30 14:43:47 -0400

    use a component-input hook to eagerly shallow-copy Pydantic models

---
+++ src/poprox_recommender/recommenders/hooks.py
@@ -0,0 +1,19 @@
+"""
+POPROX recommendation hooks.
+"""
+
+from typing import Any
+
+from lenskit.pipeline.nodes import ComponentInstanceNode
+from pydantic import BaseModel
+
+
+def shallow_copy_pydantic_model(
+    node: ComponentInstanceNode[Any], input_name: str, input_type: Any, value: Any, **context: Any
+) -> Any:
+    """
+    LensKit component-input hook to perform shallow copies of Pydantic models
+    before they are passed to components.
+    """
+    if isinstance(value, BaseModel):
+        return value.model_copy()
