e75dd3f1eef6acec629405dbe9e577749ef7f77e
Michael Ekstrand <mdekstrand@drexel.edu>
2025-06-10 17:54:36 -0400

    Update shared-data publishing and deploy (#197)
    This is a refactor of our shared-data management, with a few
    interrelated pieces to reduce our general errors:
    
    - [x] Don't track intermediate checkpoints from model training.
    - [x] Rewrite the data sharing code in Python instead of shell, reducing
    risk of weird shell errors and making it clearer how to set data access
    levels.
    - [x] Separate data fetch from deployment in `deploy.yml`, so they can
    use different DVC credentials if needed (fixing a problem Colorado has
    been facing).
    - [x] Directly use `serverless` in `deploy.yml`, to make the deploy
    workflow clearer (remove the layer of indirection of the `deploy.sh`
    options that don't match AWS / Serverless terms).
    - [x] Verify that data access levels are correctly set after adding more
    complete recommendation data outputs.
    - [x] Finish re-running evaluation after the eval refactor to get
    outputs correctly matching the new access levels.
    
    I **think** the circular dependency with trained models and tests has
    been fixed elsewhere.
    
    This adds `data` dependencies on the `wcmatch` library (because Python's
    extended glob support isn't available in 3.12) and the `invoke` library
    (Python task runner for easy interfaces to development tasks — over
    time, we can move more of our shell scripts into `invoke` tasks to
    reduce the shell we are writing).

--- shared-data.sh
+++
@@ -1,9 +0,0 @@
-# PORPOX data sharing
-# NOTE TO REVIEWERS: carefully check that any new lines are setting
-# appropriate sharing permissions.
-
-# there are 2 sharing commands: 'public' and 'shared'
-public models/*-mind
-public outputs/mind-*/*/*.parquet outputs/mind-*/*/*.csv.gz outputs/*/*/*-task.json
-shared -R outputs/mind-*/
-public -R tests/request_data
