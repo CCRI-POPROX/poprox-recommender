fadaf1fbfac6f1c8be17f98a13d87c8fb5e049b2
Karl Higley <khigley@umn.edu>
2025-06-17 09:49:08 -0400

    Compare nrms, topics, and feedback in `local_req.py`

--- scripts/local_req.py
+++ scripts/local_req.py
@@ -29,37 +29,29 @@ if __name__ == "__main__":
         "queryStringParameters": {"pipeline": "nrms_feedback_scores"},
         "isBase64Encoded": False,
     }
-    event_fm_score = {
-        "body": raw_json,
-        "queryStringParameters": {"pipeline": "fm_nrms"},
-        "isBase64Encoded": False,
-    }
 
-    # response_nrms = root(req.model_dump(), pipeline="nrms")
-    # response_nrms = RecommendationResponseV2.model_validate(response_nrms)
+    response_nrms = root(req.model_dump(), pipeline="nrms")
+    response_nrms = RecommendationResponseV2.model_validate(response_nrms)
 
-    # response_topic_score = root(req.model_dump(), pipeline="nrms_topic_scores")
-    # response_topic_score = RecommendationResponseV2.model_validate(response_topic_score)
+    response_topic_score = root(req.model_dump(), pipeline="nrms_topic_scores")
+    response_topic_score = RecommendationResponseV2.model_validate(response_topic_score)
 
     response_feedback_score = root(req.model_dump(), pipeline="nrms_feedback_scores")
     response_feedback_score = RecommendationResponseV2.model_validate(response_feedback_score)
 
-    response_fm_score = root(req.model_dump(), pipeline="fm_nrms")
-    response_fm_score = RecommendationResponseV2.model_validate(response_fm_score)
+    print("\n")
+    print(f"{event_nrms['queryStringParameters']['pipeline']}")
 
-    # print("\n")
-    # print(f"{event_nrms['queryStringParameters']['pipeline']}")
+    for idx, article in enumerate(response_nrms.recommendations.articles):
+        article_topics = extract_general_topics(article)
+        print(f"{idx + 1}. {article.headline} {article_topics}")
 
-    # for idx, article in enumerate(response_nrms.recommendations.articles):
-    #     article_topics = extract_general_topics(article)
-    #     print(f"{idx + 1}. {article.headline} {article_topics}")
+    print("\n")
+    print(f"{event_topic_score['queryStringParameters']['pipeline']}")
 
-    # print("\n")
-    # print(f"{event_topic_score['queryStringParameters']['pipeline']}")
-
-    # for idx, article in enumerate(response_topic_score.recommendations.articles):
-    #     article_topics = extract_general_topics(article)
-    #     print(f"{idx + 1}. {article.headline} {article_topics}")
+    for idx, article in enumerate(response_topic_score.recommendations.articles):
+        article_topics = extract_general_topics(article)
+        print(f"{idx + 1}. {article.headline} {article_topics}")
 
     print("\n")
     print(f"{event_feedback_score['queryStringParameters']['pipeline']}")
@@ -68,9 +60,3 @@ if __name__ == "__main__":
         article_topics = extract_general_topics(article)
         print(f"{idx + 1}. {article.headline} {article_topics}")
 
-    print("\n")
-    print(f"{event_fm_score['queryStringParameters']['pipeline']}")
-
-    for idx, article in enumerate(response_fm_score.recommendations.articles):
-        article_topics = extract_general_topics(article)
-        print(f"{idx + 1}. {article.headline} {article_topics}")
