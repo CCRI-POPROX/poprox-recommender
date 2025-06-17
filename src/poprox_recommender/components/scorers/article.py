a2b7b0e76127d507783d254abef744253f0e0a7f
Karl Higley <khigley@umn.edu>
2025-06-17 09:19:52 -0400

    Move `th.nan_to_num` inside guard clause

--- src/poprox_recommender/components/scorers/article.py
+++ src/poprox_recommender/components/scorers/article.py
@@ -11,11 +11,12 @@ class ArticleScorer(Component):
     @torch_inference
     def __call__(self, candidate_articles: CandidateSet, interest_profile: InterestProfile) -> CandidateSet:
         candidate_embeddings = candidate_articles.embeddings
-        user_embedding = torch.nan_to_num(interest_profile.embedding)
+        user_embedding = interest_profile.embedding
 
         with_scores = candidate_articles.model_copy()
 
         if user_embedding is not None:
+            user_embedding = torch.nan_to_num(user_embedding)
             pred = torch.matmul(candidate_embeddings, user_embedding.t()).squeeze()
             with_scores.scores = pred.cpu().detach().numpy()
         else:
