384ef00ffb312a73bce80e0f59de956cb9c62c4c
Aishwarya Satwani <aishwarya.satwani@colorado.edu>
2025-07-03 20:53:06 +0000

    LIP fix - removed normalization

--- src/poprox_recommender/evaluation/metrics/lip.py
+++ src/poprox_recommender/evaluation/metrics/lip.py
@@ -6,7 +6,6 @@ from poprox_concepts.domain import CandidateSet
 def least_item_promoted(reference_article_set: CandidateSet, reranked_article_set: CandidateSet, k: int = 10) -> float:
     if not reference_article_set.articles:
         return np.nan
-
     lip_rank = k
     for item in reranked_article_set.articles[:k]:
         try:
