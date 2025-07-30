0d35612f666fba2d049f9721f9447aa79136757c
Aishwarya Satwani <aishwarya.satwani@colorado.edu>
2025-07-30 21:39:11 +0000

    lip fix

--- src/poprox_recommender/evaluation/metrics/lip.py
+++ src/poprox_recommender/evaluation/metrics/lip.py
@@ -7,11 +7,12 @@ def least_item_promoted(reference_article_set: CandidateSet, reranked_article_se
     if not reference_article_set.articles:
         return np.nan
     lip_rank = k
+    reference_size = len(reference_article_set.articles)
     for item in reranked_article_set.articles[:k]:
         try:
             rank = reference_article_set.articles.index(item)
             if rank > lip_rank:
                 lip_rank = rank
         except ValueError:
-            continue
+            lip_rank = max(lip_rank, reference_size + 1)
     return lip_rank - k
