0d4f58a32fd1e3dbb08a0c652932ae71444011eb
Zannatun Naim Sristy <srist001@umn.edu>
2025-08-15 18:17:50 +0000

    calculating prelearned topic embeddings

---
+++ scripts/compute-topic-embeddings-cand.py
@@ -0,0 +1,170 @@
+import json
+import random
+
+import pandas as pd
+import torch as th
+import torch.nn as nn
+import torch.optim as optim
+from safetensors.torch import save_file
+
+from poprox_concepts import Article, CandidateSet, Entity, Mention
+from poprox_recommender.components.embedders import NRMSArticleEmbedder
+from poprox_recommender.components.topical_description import TOPIC_DESCRIPTIONS
+from poprox_recommender.paths import model_file_path, project_root
+
+
+def complete_article_generator(row, mentions_df):
+    article_mentions_df = mentions_df[mentions_df["article_id"] == row.article_id]
+    mentions = [
+        Mention(
+            mention_id=m_row.mention_id,
+            article_id=m_row.article_id,
+            source=m_row.source,
+            relevance=m_row.relevance,
+            entity=Entity(**json.loads(m_row.entity)) if m_row.entity else None,
+        )
+        for m_row in article_mentions_df.itertuples()
+    ]
+    article = Article(
+        article_id=row.article_id,
+        headline=row.headline,
+        subhead=row.subhead,
+        body=row.body,
+        published_at=row.published_at,
+        mentions=mentions,
+        source="AP",
+        external_id="",
+        raw_data=json.loads(row.raw_data) if row.raw_data else None,
+    )
+    return article
+
+
+def set_seed(seed=42):
+    random.seed(seed)
+    th.manual_seed(seed)
+    th.cuda.manual_seed_all(seed)
+    th.backends.cudnn.deterministic = True
+    th.backends.cudnn.benchmark = False
+
+
+set_seed(42)
+
+
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
+
+# fetching article and mention data
+data = project_root() / "data" / "Test"
+articles_df = pd.read_parquet(data / "articles.parquet")
+mentions_df = pd.read_parquet(data / "mentions.parquet")
+
+
+# dividing dates into history & candidate
+all_dates = sorted(articles_df["published_at"].dt.normalize().unique())
+# print(len(all_dates))
+
+history_dates = all_dates[-60:-45]
+
+
+# preparing interacted article
+history_df = articles_df[articles_df["published_at"].dt.normalize().isin(history_dates)]
+candidate_article_obj = []
+
+for row in history_df.itertuples():
+    article = complete_article_generator(row, mentions_df)
+    candidate_article_obj.append(article)
+
+candidate_article = [a.model_dump(mode="json", exclude_none=True) for a in candidate_article_obj]
+
+# breakpoint()
+
+topic_names = set()
+
+for article in candidate_article:
+    for mention in article.get("mentions", []):
+        entity = mention.get("entity")
+        topic_names.add(entity["name"])
+topic_to_idx = {t: i for i, t in enumerate(topic_names)}
+
+
+article_ids = []
+article_to_idx = {}
+
+pos_t_idx = []
+pos_a_idx = []
+
+for article in candidate_article:
+    article_id = article["article_id"]
+    if article_id not in article_to_idx:
+        article_to_idx[article_id] = len(article_ids)
+        article_ids.append(article_id)
+    a_idx = article_to_idx[article_id]
+
+    topics = set()
+    for mention in article.get("mentions", []):
+        entity = mention.get("entity")
+        topic = entity["name"]
+        if topic in topic_to_idx:
+            t_idx = topic_to_idx[topic]
+            pos_t_idx.append(t_idx)
+            pos_a_idx.append(a_idx)
+
+num_topics = len(topic_names)
+num_articles = len(article_ids)
+embed_dim = 768
+
+
+M = th.zeros((num_topics, num_articles), dtype=th.float32, device=device)
+M[pos_t_idx, pos_a_idx] = 1.0
+
+article_embedder = NRMSArticleEmbedder(
+    model_path=model_file_path("nrms-mind/news_encoder.safetensors"), device=str(device)
+)
+article_embedding_set = article_embedder(CandidateSet(articles=candidate_article))
+
+topic_emb = nn.Parameter(th.randn(num_topics, embed_dim, device=device))
+article_emb = nn.Parameter(article_embedding_set.embeddings.clone())
+article_emb.requires_grad = False
+
+
+optimizer = optim.Adam([topic_emb], lr=1e-2)
+loss_fn = nn.BCEWithLogitsLoss()
+
+n_epochs = 500
+for epoch in range(n_epochs):
+    pred = th.matmul(topic_emb, article_emb.T)
+    loss = loss_fn(pred, M)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+topic_embeddings_by_name: dict[str, th.Tensor] = {topic: topic_emb[i].detach() for topic, i in topic_to_idx.items()}
+article_embeddings_by_id: dict[str, th.Tensor] = {aid: article_emb[i].detach() for aid, i in article_to_idx.items()}
+
+
+# Tech = topic_embeddings_by_name["Climate and environment"]
+# tech_news_rec = {}
+# for article_id, embedding in article_embeddings_by_id.items():
+#     score = th.dot(Tech, embedding)
+#     for cand_art in candidate_article:
+#         if cand_art["article_id"] == article_id:
+#             news_topics = list(
+#                 {
+#                     mention["entity"]["name"]
+#                     for mention in cand_art["mentions"]
+#                     if mention["entity"]["name"] in TOPIC_DESCRIPTIONS.keys()
+#                 }
+#             )
+#             tech_news_rec[cand_art["headline"]] = [score, news_topics]
+
+# sorted_tech_news = dict(sorted(tech_news_rec.items(), key=lambda item: item[1][0], reverse=True))
+
+# for i, (headline, (score, topics)) in enumerate(list(sorted_tech_news.items())[:10], 1):
+#     print(f"{i}. {score:.4f} — {headline}")
+#     print(f"    Topics: {topics}")
+
+# breakpoint()
+
+# Write them to a safetensors file
+save_file(topic_embeddings_by_name, "topic_embeddings_cand_15_15.safetensors")
--- scripts/local_req.py
+++ scripts/local_req.py
@@ -24,20 +24,12 @@ if __name__ == "__main__":
         "queryStringParameters": {"pipeline": "nrms_topic_scores"},
         "isBase64Encoded": False,
     }
-    event_feedback_score = {
-        "body": raw_json,
-        "queryStringParameters": {"pipeline": "nrms_article_feedback"},
-        "isBase64Encoded": False,
-    }
 
     response_nrms = root(req.model_dump(), pipeline="nrms")
     response_nrms = RecommendationResponseV2.model_validate(response_nrms)
 
     response_topic_score = root(req.model_dump(), pipeline="nrms_topic_scores")
     response_topic_score = RecommendationResponseV2.model_validate(response_topic_score)
-
-    response_feedback_score = root(req.model_dump(), pipeline="nrms_article_feedback")
-    response_feedback_score = RecommendationResponseV2.model_validate(response_feedback_score)
 
     print("\n")
     print(f"{event_nrms['queryStringParameters']['pipeline']}")
@@ -50,12 +42,5 @@ if __name__ == "__main__":
     print(f"{event_topic_score['queryStringParameters']['pipeline']}")
 
     for idx, article in enumerate(response_topic_score.recommendations.articles):
-        article_topics = extract_general_topics(article)
-        print(f"{idx + 1}. {article.headline} {article_topics}")
-
-    print("\n")
-    print(f"{event_feedback_score['queryStringParameters']['pipeline']}")
-
-    for idx, article in enumerate(response_feedback_score.recommendations.articles):
         article_topics = extract_general_topics(article)
         print(f"{idx + 1}. {article.headline} {article_topics}")
