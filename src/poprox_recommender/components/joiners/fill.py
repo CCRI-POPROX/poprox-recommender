17db9c3d610a795b0f1b0d3b2a6a822aba82746f
Karl Higley <khigley@umn.edu>
2025-10-21 10:00:40 -0400

    Remove `FillCandidates` component

--- src/poprox_recommender/components/joiners/fill.py
+++ src/poprox_recommender/components/joiners/fill.py
@@ -4,43 +4,12 @@ from lenskit.pipeline import Component
 from lenskit.pipeline.types import Lazy
 from pydantic import BaseModel
 
-from poprox_concepts import CandidateSet
 from poprox_concepts.domain import RecommendationList
 
 
 class FillConfig(BaseModel):
     num_slots: int
     deduplicate: bool = True
-
-
-class FillCandidates(Component):
-    config: FillConfig
-
-    def __call__(self, recs1: CandidateSet, recs2: Lazy[CandidateSet]) -> RecommendationList:
-        articles = recs1.articles
-
-        if self.config.deduplicate:
-            # Track the articles by their article_id
-            existing_articles = {(article.article_id) for article in articles}
-
-            # Add articles from candidates2 only if they are not duplicates
-            if len(articles) < self.config.num_slots:
-                new_articles = []
-                for article in recs2.get().articles:
-                    # Check if the article is a duplicate based on article_id
-                    if (article.article_id) not in existing_articles:
-                        new_articles.append(article)
-                        existing_articles.add((article.article_id))  # Avoid future duplicates
-                    # Stop if we have enough articles
-                    if len(articles) + len(new_articles) >= self.config.num_slots:
-                        break
-
-                articles = articles + new_articles
-        else:
-            articles = articles + recs2.get().articles
-
-        # Return the resulting RecommendationList, limiting the size to num_slots
-        return CandidateSet(articles=articles[: self.config.num_slots])
 
 
 class FillRecs(Component):
