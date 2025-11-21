6a2715b320f773c2e962bf2a79805477a70bdfa6
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-21 15:35:47 -0500

    create POPROX indexes

--- data/poprox-ingest.sql
+++ data/poprox-ingest.sql
@@ -80,11 +80,14 @@ INSERT INTO newsletter_articles
 SELECT
     newsletter_id, article_id, position, headline, subhead
 FROM 'POPROX/newsletters.parquet';
+CREATE INDEX newsletter_article_idx ON newsletter_articles (article_id);
 
 INSERT INTO clicks
 SELECT account_id, newsletter_id, article_id, clicked_at
 FROM 'POPROX/clicks.parquet'
 ORDER BY account_id, newsletter_id;
+CREATE INDEX click_account_idx ON clicks (account_id);
+CREATE INDEX click_newsletter_idx ON clicks (newsletter_id);
 
 INSERT INTO interests
 SELECT account_id, entity_id, entity_name, preference
@@ -102,6 +105,7 @@ INSERT INTO candidate_article_mentions
 SELECT mention_id, article_id, source, relevance, entity
 FROM 'POPROX/article_mentions.parquet'
 ORDER BY article_id;
+CREATE INDEX candidate_article_mention_article_idx ON candidate_article_mentions (article_id);
 
 INSERT INTO clicked_articles
 SELECT
@@ -114,3 +118,5 @@ INSERT INTO clicked_article_mentions
 SELECT mention_id, article_id, source, relevance, entity
 FROM 'POPROX/clicked/article_mentions.parquet'
 ORDER BY article_id;
+
+CREATE INDEX clicked_article_mention_article_idx ON clicked_article_mentions (article_id);
