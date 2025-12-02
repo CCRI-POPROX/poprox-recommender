4daa96e8098d9de53b422d489744393155bc9709
Michael Ekstrand <mdekstrand@drexel.edu>
2025-12-02 17:47:44 -0500

    simplify newsletter candidate query

--- data/poprox-post.sql
+++ data/poprox-post.sql
@@ -1,6 +1,5 @@
 -- Pre-compute candidate lists for newsletter dates.
 -- Can be replaced later with a table from updated export.
 CREATE TABLE newsletter_candidates AS
-SELECT DISTINCT newsletter_date, article_id
-FROM newsletters n, candidate_articles a
-WHERE n.created_at::DATE = a.created_at::DATE;
+SELECT DISTINCT a.created_at::DATE AS newsletter_date, article_id
+FROM candidate_articles a;
