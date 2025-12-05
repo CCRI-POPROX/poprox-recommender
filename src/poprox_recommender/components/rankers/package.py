dd7edb1680ec69d8935a7f2bc602361e1258d0f1
Sushobhan Parajuli <sp3886@gmail.com>
2025-12-05 21:08:56 +0000

    update to a list of article packages

--- src/poprox_recommender/components/rankers/package.py
+++ src/poprox_recommender/components/rankers/package.py
@@ -15,18 +15,20 @@ class PackageConfig(BaseModel):
 class PackageRanker(Component):
     config: PackageConfig
 
-    def __call__(self, candidate_articles: CandidateSet, article_package: ArticlePackage) -> ImpressedSection:
+    def __call__(self, candidate_articles: CandidateSet, article_packags: list[ArticlePackage]) -> ImpressedSection:
         article_lookup = {article.article_id: article for article in candidate_articles.articles}
 
-        selected_articles = [
-            article_lookup[article_id] for article_id in article_package.article_ids if article_id in article_lookup
-        ]
+        selected_articles = []
+        for package in article_packags:
+            selected_articles.extend(
+                article_lookup[article_id] for article_id in package.article_ids if article_id in article_lookup
+            )
 
         logger.debug(
-            "PackageRanker selected %d of %d candidate articles for package '%s'",
+            "PackageRanker selected %d of %d candidate articles from'%d' packages",
             len(selected_articles),
             len(candidate_articles.articles),
-            self.config.package_name,
+            len(article_packags),
         )
 
         return ImpressedSection.from_articles(selected_articles)
