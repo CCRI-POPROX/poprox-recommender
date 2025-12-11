adfd79ef2333fb39e84b363713e91a6e149025ba
mahamudul42 <hasan181@umn.edu>
2025-12-11 04:33:30 +0000

    updating init for the top news filter

--- src/poprox_recommender/components/filters/__init__.py
+++ src/poprox_recommender/components/filters/__init__.py
@@ -1,3 +1,4 @@
 from poprox_recommender.components.filters.topic import TopicFilter
+from poprox_recommender.components.filters.top_news import TopNewsFilter
 
-__all__ = ["TopicFilter"]
+__all__ = ["TopicFilter", "TopNewsFilter"]
