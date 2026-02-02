5010f3d3c8ec052d8387a377966e50fe9efb14df
mahamudul42 <hasan181@umn.edu>
2026-02-02 23:32:05 +0000

    adding impression filtering in init.

--- src/poprox_recommender/components/filters/__init__.py
+++ src/poprox_recommender/components/filters/__init__.py
@@ -1,7 +1,9 @@
+from poprox_recommender.components.filters.impression import ImpressionFilter
 from poprox_recommender.components.filters.package import PackageFilter, PackageFilterConfig
 from poprox_recommender.components.filters.topic import TopicFilter
 
 __all__ = [
+    "ImpressionFilter",
     "TopicFilter",
     "PackageFilter",
     "PackageFilterConfig",
