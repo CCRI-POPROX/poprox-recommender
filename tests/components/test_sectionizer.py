be3ded7d8a242d369451a6935d39cc7f867c77eb
Karl Higley <khigley@umn.edu>
2026-02-05 14:18:45 -0500

    Add tests for `filter_using_packages()` function
    These tests just check the basic functionality to make sure that the filter is actually filtering and that it's doing so using the articles contained in all supplied packages.

--- tests/components/test_sectionizer.py
+++ tests/components/test_sectionizer.py
@@ -1,7 +1,12 @@
 from uuid import UUID, uuid4
 
 from poprox_concepts.domain import AccountInterest, Article, ArticlePackage, CandidateSet, Entity, InterestProfile
-from poprox_recommender.components.rankers.sectionizer import Sectionizer, SectionizerConfig, select_from_candidates
+from poprox_recommender.components.rankers.sectionizer import (
+    Sectionizer,
+    SectionizerConfig,
+    filter_using_packages,
+    select_from_candidates,
+)
 
 
 def make_interest_profile(topic_ids: list[UUID]) -> InterestProfile:
@@ -140,3 +145,41 @@ def test_select_from_candidates_excluding_without_scores():
     selected = select_from_candidates(candidates, 3, excluding=[articles[0].article_id])
     assert len(selected) == 3
     assert [a.article_id for a in selected] == [articles[1].article_id, articles[2].article_id, articles[3].article_id]
+
+
+def test_filter_using_one_package():
+    articles = [
+        Article(article_id=uuid4(), headline="Article 1"),
+        Article(article_id=uuid4(), headline="Article 2"),
+        Article(article_id=uuid4(), headline="Article 3"),
+        Article(article_id=uuid4(), headline="Article 4"),
+    ]
+    candidates = CandidateSet(articles=articles)
+
+    package_article_ids = [articles[1].article_id, articles[3].article_id]
+    package = ArticlePackage(title="half the articles", source="test", article_ids=package_article_ids)
+
+    filtered = filter_using_packages(candidates, [package])
+    filtered_ids = [a.article_id for a in filtered.articles]
+
+    for article_id in package_article_ids:
+        assert article_id in filtered_ids
+
+
+def test_filter_using_multiple_packages():
+    articles = [
+        Article(article_id=uuid4(), headline="Article 1"),
+        Article(article_id=uuid4(), headline="Article 2"),
+        Article(article_id=uuid4(), headline="Article 3"),
+        Article(article_id=uuid4(), headline="Article 4"),
+    ]
+    candidates = CandidateSet(articles=articles)
+
+    package_1 = ArticlePackage(title="half the articles", source="test", article_ids=[articles[1].article_id])
+    package_2 = ArticlePackage(title="half the articles", source="test", article_ids=[articles[3].article_id])
+
+    filtered = filter_using_packages(candidates, [package_1, package_2])
+    filtered_ids = [a.article_id for a in filtered.articles]
+
+    for article_id in package_1.article_ids + package_2.article_ids:
+        assert article_id in filtered_ids
