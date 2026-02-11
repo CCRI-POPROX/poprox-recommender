f6285c041e87ebe03053df7b254e969cac937cc1
Sushobhan Parajuli <sp3886@gmail.com>
2026-02-11 20:29:03 +0000

    change num_recs in remaing tests

--- tests/web_service/test_no_clicks_no_onboarding.py
+++ tests/web_service/test_no_clicks_no_onboarding.py
@@ -27,7 +27,7 @@ def test_no_clicks_no_onboarding(service, mind_data, pipeline):  # noqa: F811
     request_generator.add_candidates(150)
     request_generator.add_clicks(num_clicks=0, num_days=0)
     request_generator.add_topics(default_topics)  # pick the default topics when no onboarding
-    num_recs = 15 if pipeline == "nrms_sections" else 10
+    num_recs = 15
     request_generator.set_num_recs(num_recs)
 
     req_body = request_generator.get_request()
