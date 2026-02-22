7ee47dbd62a9c2114491f8d97351c137d21dbd76
Karl Higley <khigley@umn.edu>
2026-02-22 12:08:39 -0500

    Change the default recommender pipeline to `nrms_sections`

--- src/poprox_recommender/recommenders/configurations/__init__.py
+++ src/poprox_recommender/recommenders/configurations/__init__.py
@@ -3,7 +3,7 @@ Module definitions for the different recommender pipelines defined in the POPROX
 recommender service.
 """
 
-DEFAULT_PIPELINE = "nrms_topics_fb_filter"
+DEFAULT_PIPELINE = "nrms_sections"
 """
 The default recommender pipeline. Can be overridden by the
 :envvar:`POPROX_DEFAULT_PIPELINE` environment variable.
