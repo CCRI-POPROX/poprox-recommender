2640dbb15ef1718457882e51031c780408b5834b
Karl Higley <khigley@umn.edu>
2026-03-13 15:37:23 -0400

    Move `select_from_candidates` to a separate file

--- src/poprox_recommender/components/rankers/sectionizer.py
+++ src/poprox_recommender/components/rankers/sectionizer.py
@@ -1,14 +1,14 @@
 import hashlib
 import logging
 from datetime import date
-from uuid import UUID
 
 import numpy as np
 from lenskit.pipeline import Component
 from pydantic import BaseModel
 
-from poprox_concepts.domain import Article, ArticlePackage, CandidateSet, Entity, ImpressedSection, InterestProfile
+from poprox_concepts.domain import ArticlePackage, CandidateSet, Entity, ImpressedSection, InterestProfile
 from poprox_recommender.components.filters.topic import TopicFilter
+from poprox_recommender.components.sections.base import select_from_candidates
 
 logger = logging.getLogger(__name__)
 
@@ -272,21 +272,3 @@ def select_from_packages(candidate_articles: CandidateSet, packages: list[Articl
         filtered.scores = None
 
     return filtered
-
-
-def select_from_candidates(candidates: CandidateSet, num_articles: int, excluding: set[UUID] = None) -> list[Article]:
-    excluding = excluding or set()
-
-    if hasattr(candidates, "scores") and candidates.scores is not None:
-        # rank candidates by score if scores are available
-        sorted_indices = np.argsort(np.array(candidates.scores))[::-1]
-        ranked_articles = [
-            candidates.articles[int(i)]
-            for i in sorted_indices
-            if candidates.articles[int(i)].article_id not in excluding
-        ][:num_articles]
-    else:
-        # otherwise select from the top of the list of candidates preserving order
-        ranked_articles = [a for a in candidates.articles if a.article_id not in excluding][:num_articles]
-
-    return ranked_articles
