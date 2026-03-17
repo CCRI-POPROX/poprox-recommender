6aca03a6ab401688d33ad0d29ab3f88f489f50c7
Karl Higley <khigley@umn.edu>
2026-03-17 14:04:56 -0400

    Move tests for `TopStoryCandidates` selector to a separate file
    Move `TopStoryCandidates` to a separate file

--- src/poprox_recommender/components/sections/top_news.py
+++ src/poprox_recommender/components/sections/top_news.py
@@ -3,6 +3,7 @@ from datetime import date
 
 from lenskit.pipeline import Component
 from pydantic import BaseModel
+from recommender.src.poprox_recommender.components.selectors.top_news import TopStoryCandidates
 
 from poprox_concepts.domain import ArticlePackage, CandidateSet, ImpressedSection, InterestProfile
 from poprox_recommender.components.filters.topic import TopicFilter
@@ -50,34 +51,3 @@ class PersonalizedTopNews(Component):
             sections.append(top_section)
 
         return sections
-
-
-class TopStoryCandidates(Component):
-    def __call__(self, candidate_articles: CandidateSet, article_packages: list[ArticlePackage]) -> CandidateSet:
-        article_index_lookup = {article.article_id: i for i, article in enumerate(candidate_articles.articles)}
-        selected_articles = []
-        selected_indices = []
-
-        package_article_ids = set(article_id for package in article_packages for article_id in package.article_ids)
-
-        for article_id in package_article_ids:
-            if article_id in article_index_lookup:
-                idx = article_index_lookup[article_id]
-                selected_articles.append(candidate_articles.articles[idx])
-                selected_indices.append(idx)
-
-        logger.debug(
-            "TopStoryCandidates selected %d of %d candidate articles using %s packages",
-            len(selected_articles),
-            len(candidate_articles.articles),
-            ", ".join([p.title for p in article_packages]),
-        )
-
-        filtered = CandidateSet(articles=selected_articles)
-        scores = getattr(candidate_articles, "scores", None)
-        if scores is not None:
-            filtered.scores = [scores[i] for i in selected_indices]
-        else:
-            filtered.scores = None
-
-        return filtered
