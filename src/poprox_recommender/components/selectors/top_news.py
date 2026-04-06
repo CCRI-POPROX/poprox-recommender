e8756c98662f651c444a9dff21c3f4e1950de1ed
Karl Higley <khigley@umn.edu>
2026-04-06 12:07:57 -0400

    Add a candidate selector that returns AP's Top General News stories

--- src/poprox_recommender/components/selectors/top_news.py
+++ src/poprox_recommender/components/selectors/top_news.py
@@ -1,4 +1,5 @@
 import logging
+from uuid import UUID
 
 from lenskit.pipeline import Component
 
@@ -36,3 +37,25 @@ class TopStoryCandidates(Component):
             filtered.scores = None
 
         return filtered
+
+
+class TopGeneralNewsCandidates(Component):
+    def __call__(self, candidate_articles: CandidateSet, article_packages: list[ArticlePackage]) -> CandidateSet:
+        general_news_uuid = UUID("72bb7674-7bde-4f3e-a351-ccdeae888502")
+
+        # Find article ids from the General News package
+        general_news_package = next(
+            package for package in article_packages if package.seed.entity_id == general_news_uuid
+        )
+        article_ids = general_news_package.article_ids
+
+        # Iterate through the candidates to find the right articles and build an ordered list
+        selected = []
+        for article_id in article_ids:
+            article = next(article for article in candidate_articles.articles if article.article_id == article_id)
+            if article:
+                selected.append(article)
+
+        # This component *does not* preserve relevance scores so that downstream
+        # TopkRanker will maintain the order that's constructed here
+        return CandidateSet(articles=selected)
