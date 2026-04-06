b01928bfc5a75378bd2470c7de392af68650cf62
Karl Higley <khigley@umn.edu>
2026-04-06 12:22:00 -0400

    Update tests to expect longer newsletter from the new pipeline config

--- tests/web_service/test_basic_request.py
+++ tests/web_service/test_basic_request.py
@@ -33,7 +33,7 @@ def test_basic_request(service, mind_data, pipeline):  # noqa: F811
             "Oddities",
         ]
     )
-    num_recs = 15 if pipeline == "nrms_sections" else 10
+    num_recs = 15 if pipeline.startswith("nrms_sections") else 10
     request_generator.set_num_recs(num_recs)
 
     req_body = request_generator.get_request()
