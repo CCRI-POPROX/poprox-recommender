7043aed853120ef98b6e492147ae65097938fb27
Karl Higley <khigley@umn.edu>
2026-04-06 13:33:01 -0400

    Add a sectioned recommender with non-personalized top news section (#302)

--- tests/web_service/test_basic_request.py
+++ tests/web_service/test_basic_request.py
@@ -27,13 +27,14 @@ def test_basic_request(service, mind_data, pipeline):  # noqa: F811
     request_generator.add_clicks(num_clicks=37, num_days=7)
     request_generator.add_topics(
         [
+            "General News",
             "Science",
             "Technology",
             "Sports",
             "Oddities",
         ]
     )
-    num_recs = 15 if pipeline == "nrms_sections" else 10
+    num_recs = 15 if pipeline.startswith("nrms_sections") else 10
     request_generator.set_num_recs(num_recs)
 
     req_body = request_generator.get_request()
